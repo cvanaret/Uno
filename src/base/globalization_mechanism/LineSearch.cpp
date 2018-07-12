526b4de3d79bc47bdfc6aad84f38876e310fbc7c
vanaret <cvanaret@anl.gov>
2018-07-12 19:31:13 +0200

    step_length member of LineSearch

--- src/base/globalization_mechanism/LineSearch.cpp
+++ src/base/globalization_mechanism/LineSearch.cpp
@@ -10,24 +10,18 @@ Iterate LineSearch::compute_iterate(Problem& problem, Iterate& current_iterate) 
 	/* compute a trial direction */
 	LocalSolution solution = this->globalization_strategy.compute_step(problem, current_iterate, INFINITY);
 	
-	///* fail if direction is not a descent direction */
-	//std::map<int,double> objective_gradient = problem.objective_sparse_gradient(current_iterate.x);
-	//if (0. < dot(solution.x, objective_gradient)) {
-		//throw std::out_of_range("Line search direction is not a descent direction");
-	//}
-
 	/* length follows the following sequence: 1, ratio, ratio^2, ratio^3, ... */
-	double step_length = 1.;
+	this->step_length = 1.;
 	bool is_accepted = false;
 	this->number_iterations = 0;
 	
 	while (!this->termination(is_accepted, this->number_iterations)) {
 		this->number_iterations++;
-		DEBUG << "\n\tLINE SEARCH iteration " << this->number_iterations << ", step_length " << step_length << "\n";
+		DEBUG << "\n\tLINE SEARCH iteration " << this->number_iterations << ", step_length " << this->step_length << "\n";
 			
 		try {
 			/* check whether the trial step is accepted */
-			is_accepted = this->globalization_strategy.check_step(problem, current_iterate, solution, step_length);
+			is_accepted = this->globalization_strategy.check_step(problem, current_iterate, solution, this->step_length);
 		}
 		catch (const std::invalid_argument& e) {
 			WARNING << RED << e.what() << RESET "\n";
@@ -35,8 +29,8 @@ Iterate LineSearch::compute_iterate(Problem& problem, Iterate& current_iterate) 
 		}
 		
 		if (is_accepted) {
-			DEBUG << CYAN "LS trial point accepted\n" RESET;
 			/* print summary */
+			DEBUG << CYAN "LS trial point accepted\n" RESET;
 			// TODO handle the case where the solution is a new point: what???
 			INFO << "minor: " << std::fixed << this->number_iterations << "\t";
 			INFO << "step length: " << step_length << "\t";
@@ -44,7 +38,7 @@ Iterate LineSearch::compute_iterate(Problem& problem, Iterate& current_iterate) 
 		}
 		else {
 			/* decrease the step length */
-			step_length *= this->ratio;
+			this->step_length *= this->ratio;
 		}
 	}
 	return current_iterate;
@@ -58,8 +52,8 @@ bool LineSearch::termination(bool is_accepted, int iteration) {
 		throw std::out_of_range("Line-search iteration limit reached");
 	}
 	/* step length gets too small */
-	//if (step_length < 1e-16) {
-	//	throw std::out_of_range("Step length became too small");
-	//}
+	if (this->step_length < 1e-16) {
+		throw std::out_of_range("Step length became too small");
+	}
 	return false;
 }
--- src/base/globalization_mechanism/LineSearch.hpp
+++ src/base/globalization_mechanism/LineSearch.hpp
@@ -23,6 +23,7 @@ class LineSearch: public GlobalizationMechanism {
          */
 		Iterate compute_iterate(Problem& problem, Iterate& current_iterate);
 		
+		double step_length;
 		/* ratio of step length update in ]0, 1[ */
 		double ratio;
 		
