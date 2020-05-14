cc4328b38bef28852bac04d1ae1c63026b10aa30
vanaret <charlie.vanaret@enseeiht.fr>
2020-05-14 18:28:54 +0200

    Made wdotd.f easier to maintain

--- src/solvers/QP/wdotd.f
+++ src/solvers/QP/wdotd.f
@@ -1,9 +1,9 @@
 C     hristen this file wdotd.f
 
-      subroutine wdotd (n, d, ws, lws, v)
+      subroutine wdotd (n, x, ws, lws, result)
 
 c     ==========================================================
-c     Computes W.d where W is Hessian and d is a vector for AMPL
+c     Computes result = W.x where W is Hessian and x is a vector for AMPL
 c     Assumes v=0 on entry (OK, if called from gdotx, see QPsolve*.f)
 c     ==========================================================
 
@@ -11,45 +11,44 @@ c     ==========================================================
 
 c     ... declaration of passed parameters
       integer          n, lws(0:*)
-      double precision d(n), v(n), ws(*)
+      double precision x(n), result(n), ws(*)
 
 c     ... declaration of internal variables
-      integer i, j, pjp, row
+      integer i, j, k, footer_start
 
+c     inertia control for diagonal terms
       double precision alpha
       common /kktalphac/ alpha
 
 c     ========================  procedure body  =========================
 
-c     ... form v = W.d from sparse, upper triangular Hessian
-      pjp = lws(0)
+c     ... form result = W.x from sparse, upper triangular Hessian
+      footer_start = lws(0)
       do i=1,n
-         do j=lws(pjp+i-1),lws(pjp+i)-1
-            row  = lws(j)
-            v(i) = v(i) + ws(j)*d(row)
-            if (row.ne.i) then
-               v(row) = v(row) + ws(j)*d(i)
+         do k=lws(footer_start + i - 1), lws(footer_start + i)-1
+            j = lws(k)
+            result(i) = result(i) + ws(k)*x(j)
+            if (j.ne.i) then
+c               off-diagonal term
+               result(j) = result(j) + ws(k)*x(i)
             else
-               v(i)   = v(i) + alpha*d(row)
+c               diagonal term
+               result(i) = result(i) + alpha*x(j)
             endif
          enddo
       enddo
-
-c     print * '  d = ',(d(i),i=1,n)
-c     print * 'W.d = ',(v(i),i=1,n)
-
       return
       end
 
 c     ******************************************************************
 
-      subroutine gdotx (n, x, ws, lws, v)
+      subroutine gdotx (n, x, ws, lws, result)
 
       implicit none
 
 c     ... declaration of passed parameters
       integer n, lws(*)
-      double precision    x(n), v(n), ws(*)
+      double precision    x(n), result(n), ws(*)
 
 c     ... declaration of internal variables
       integer i
@@ -60,9 +59,9 @@ c     ... storage map for hessian and scale_mode
 
 c     ========================  procedure body  =========================
 
-c     ... set v = 0
+c     ... set result = 0
       do i=1,n
-         v(i) = 0.D0
+         result(i) = 0.D0
       enddo
 
 c     ... allow for scaling of variables 
@@ -73,12 +72,12 @@ c     ... allow for scaling of variables
       endif
 
 c     ... form v = W.d from sparse, upper triangular Hessian
-      call Wdotd (n, x, ws(phe+1), lws, v)
+      call Wdotd (n, x, ws(phe+1), lws, result)
 
 c     ... allow for scaling of variables 
       if ((scale_mode.eq.1).or.(scale_mode.eq.3)) then
          do i=1,n
-            v(i) = v(i) * ws(i)
+            result(i) = result(i) * ws(i)
             x(i) = x(i) / ws(i)
          enddo
       endif
@@ -90,12 +89,12 @@ c     ******************************************************************
       implicit double precision (a-h,o-z)
       dimension a(*),la(0:*),y(*)
 c     ========================  procedure body  =========================
-c     saxpy with column i of A
-      if(s.eq.0.D0)return
-      jp=la(0)+i
-      do j=la(jp),la(jp+1)-1
-         ir=la(j)
-         y(ir)=y(ir)+s*a(j)
+c     saxpy with column i of A: y + s*A_{i, :}
+      if(s.eq.0.D0) return
+      j_column_start = la(0) + i
+      do j = la(j_column_start), la(j_column_start+1)-1
+         i_variable = la(j)
+         y(i_variable) = y(i_variable) + s*a(j)
       enddo
       return
       end
@@ -105,11 +104,12 @@ c     **************************** E N D *********************************
       implicit double precision (a-h,o-z)
       dimension a(*),la(0:*),x(*)
       DOUBLE PRECISION daiscpr2
-      daiscpr2=dble(b)
-      jp=la(0)+i
-      do j=la(jp),la(jp+1)-1
-         ir=la(j)
-         daiscpr2=daiscpr2+dble(x(ir))*dble(a(j))
+c     dot product of x and row i of A
+      daiscpr2 = dble(b)
+      j_column_start = la(0) + i
+      do j = la(j_column_start), la(j_column_start+1)-1
+         i_variable = la(j)
+         daiscpr2 = daiscpr2 + dble(x(i_variable))*dble(a(j))
       enddo
       return
       end
