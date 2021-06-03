40d3bddeb5ceb195008e4086b2eff7e7c2cf1e21
Charlie Vanaret <vanaret@zib.de>
2021-06-03 12:23:59 +0200

    Commented out unused parameters in FeasibilityRestoration.cpp (TBD)

--- src/base/feasibility/FeasibilityRestoration.cpp
+++ src/base/feasibility/FeasibilityRestoration.cpp
@@ -1,13 +1,14 @@
 #include "FeasibilityRestoration.hpp"
 
-FeasibilityRestoration::FeasibilityRestoration(Subproblem& subproblem): FeasibilityStrategy(subproblem) {
+FeasibilityRestoration::FeasibilityRestoration(Subproblem& subproblem) : FeasibilityStrategy(subproblem) {
 }
 
-std::vector<Direction> FeasibilityRestoration::compute_feasible_directions(Problem& problem, Iterate& current_iterate, double
-trust_region_radius) {
+std::vector<Direction> FeasibilityRestoration::compute_feasible_directions(Problem& /*problem*/, Iterate& /*current_iterate*/, double
+/*trust_region_radius*/) {
    return std::vector<Direction>();
 }
 
-double FeasibilityRestoration::compute_predicted_reduction(Problem& problem, Iterate& current_iterate, Direction& direction, double step_length) {
+double FeasibilityRestoration::compute_predicted_reduction(Problem& /*problem*/, Iterate& /*current_iterate*/, Direction& /*direction*/,
+      double /*step_length*/) {
    return 0.;
 }
