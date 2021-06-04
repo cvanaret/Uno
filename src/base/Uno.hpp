65fa3ab2b33517a6b68ce1149e7818fd493e86a4
cvanaret <vanaret@math.tu-berlin.de>
2021-06-04 16:34:01 +0200

    Uno: added a function to insert statistics into the Statistics object

--- src/base/Uno.hpp
+++ src/base/Uno.hpp
@@ -50,9 +50,11 @@ public:
 
 private:
    static Statistics create_statistics();
+   void add_statistics();
    [[nodiscard]] bool termination_criterion_(TerminationStatus is_optimal, int iteration) const;
    TerminationStatus
    compute_termination_status_(Problem& problem, Iterate& current_iterate, double step_norm, double objective_multiplier) const;
+   void add_statistics(Statistics& statistics, const Iterate& new_iterate, int major_iterations);
 };
 
 #endif // UNO_H
