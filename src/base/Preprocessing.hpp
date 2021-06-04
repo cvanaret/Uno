d329fc7af1c39fbecdcbd29493552903dfa09ba4
Charlie Vanaret <vanaret@zib.de>
2021-06-04 16:30:09 +0200

    Moved preprocessing (feasibility wrt linear constraints) to its own class

---
+++ src/base/Preprocessing.hpp
@@ -0,0 +1,13 @@
+#ifndef PREPROCESSING_H
+#define PREPROCESSING_H
+
+#include <vector>
+#include "Problem.hpp"
+#include "Constraint.hpp"
+
+class Preprocessing {
+public:
+   static void apply(Problem& problem, std::vector<double>& x, Multipliers& multipliers);
+};
+
+#endif //PREPROCESSING_H
