74069fd32f7583cd72ab02cc36a1f90c942bfdb2
Charlie Vanaret <vanaret@zib.de>
2021-06-06 20:04:37 +0200

    QPSolver: minor refactoring

--- src/solvers/QP/LPSolver.hpp
+++ src/solvers/QP/LPSolver.hpp
@@ -11,9 +11,9 @@
 class LPSolver {
 public:
 
-    virtual ~LPSolver() {
-    };
-    virtual Direction solve_LP(std::vector<Range>& variables_bounds, std::vector<Range>& constraints_bounds, SparseVector& linear_objective, std::vector<SparseVector>& constraints_jacobian, std::vector<double>& x) = 0;
+   virtual ~LPSolver() = default;
+   virtual Direction solve_LP(std::vector<Range>& variables_bounds, std::vector<Range>& constraints_bounds, SparseVector& linear_objective,
+         std::vector<SparseVector>& constraints_jacobian, std::vector<double>& x) = 0;
 };
 
 #endif // LPSOLVER_H
