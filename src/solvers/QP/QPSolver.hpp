45ba02703d265108f88173757c38af4d487ef2d7
cvanaret <vanaret@math.tu-berlin.de>
2021-06-06 20:04:37 +0200

    QPSolver: minor refactoring

--- src/solvers/QP/QPSolver.hpp
+++ src/solvers/QP/QPSolver.hpp
@@ -13,10 +13,11 @@
 class QPSolver : public LPSolver {
 public:
 
-    virtual ~QPSolver() {
-    };
-    virtual Direction solve_QP(std::vector<Range>& variables_bounds, std::vector<Range>& constraints_bounds, SparseVector& linear_objective, std::vector<SparseVector>& constraints_jacobian, CSCMatrix& hessian, std::vector<double>& x0) = 0;
-    virtual Direction solve_LP(std::vector<Range>& variables_bounds, std::vector<Range>& constraints_bounds, SparseVector& linear_objective, std::vector<SparseVector>& constraints_jacobian, std::vector<double>& x0) = 0;
+   ~QPSolver() override = default;
+   virtual Direction solve_QP(std::vector<Range>& variables_bounds, std::vector<Range>& constraints_bounds, SparseVector& linear_objective,
+         std::vector<SparseVector>& constraints_jacobian, CSCMatrix& hessian, std::vector<double>& x0) = 0;
+   Direction solve_LP(std::vector<Range>& variables_bounds, std::vector<Range>& constraints_bounds, SparseVector& linear_objective,
+         std::vector<SparseVector>& constraints_jacobian, std::vector<double>& x0) override = 0;
 };
 
 #endif // QPSOLVER_H
