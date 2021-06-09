4de49662114e92e1b011c580824cb705e268d10c
Charlie Vanaret <vanaret@zib.de>
2021-06-09 17:14:24 +0200

    Created linear_algebra directory

--- src/base/SparseVector.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef SPARSEVECTOR_H
-#define SPARSEVECTOR_H
-
-#include <map>
-#include <unordered_map>
-using SparseVector = std::unordered_map<unsigned int, double>;
-
-#endif // SPARSEVECTOR_H
