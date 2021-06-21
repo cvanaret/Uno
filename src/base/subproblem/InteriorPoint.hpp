bc00a9fca2b779168ad8f44280fc813e785ac61d
Charlie Vanaret <vanaret@zib.de>
2021-06-21 20:18:00 +0200

    IPM: preallocated vectors for bound duals

--- src/base/subproblem/InteriorPoint.hpp
+++ src/base/subproblem/InteriorPoint.hpp
@@ -30,7 +30,7 @@ public:
          bool scale_residuals);
 
    Iterate generate_initial_point(Statistics& statistics, const Problem& problem, std::vector<double>& x, Multipliers& multipliers) override;
-   void generate(const Problem& problem, const Iterate& current_iterate, double objective_multiplier, double trust_region_radius) override;
+   void generate(const Problem& problem, Iterate& current_iterate, double objective_multiplier, double trust_region_radius) override;
    void update_objective_multiplier(const Problem& problem, const Iterate& current_iterate, double objective_multiplier) override;
 
    Direction compute_direction(Statistics& statistics, const Problem& problem, Iterate& current_iterate) override;
@@ -50,7 +50,6 @@ private:
    std::set<size_t> upper_bounded_variables; /* indices of the upper-bounded variables */
 
    bool force_symbolic_factorization{true};
-   std::vector<double> rhs;
    double inertia_hessian;
    double inertia_hessian_last_;
    double inertia_constraints;
@@ -58,23 +57,28 @@ private:
    size_t iteration;
    size_t number_factorizations_;
 
-   void update_barrier_parameter(const Problem& problem, Iterate& current_iterate);
+   /* preallocated vectors */
+   std::vector<double> rhs;
+   std::vector<double> lower_delta_z;
+   std::vector<double> upper_delta_z;
+
+   void update_barrier_parameter(Iterate& current_iterate);
    void set_variables_bounds(const Problem& problem, const Iterate& current_iterate, double trust_region_radius) override;
    void factorize(COOMatrix& kkt_matrix, FunctionType problem_type);
    double compute_barrier_directional_derivative(const std::vector<double>& solution);
    double evaluate_barrier_function(const Problem& problem, Iterate& iterate);
    double compute_primal_length(const Iterate& current_iterate, const std::vector<double>& ipm_solution, double tau);
-   static double compute_dual_length(const Iterate& current_iterate, double tau, const std::vector<double>& lower_delta_z, const std::vector<double>&
-         upper_delta_z);
+   double compute_dual_length(const Iterate& current_iterate, double tau);
    COOMatrix assemble_kkt_matrix(const Problem& problem, Iterate& current_iterate);
    void modify_inertia(COOMatrix& kkt_matrix, size_t size_first_block, size_t size_second_block, FunctionType problem_type);
-   void generate_kkt_rhs(const Problem& problem, const std::vector<double>& current_primals);
-   std::vector<double> compute_lower_bound_dual_displacements(const Iterate& current_iterate, const std::vector<double>& solution);
-   std::vector<double> compute_upper_bound_dual_displacements(const Iterate& current_iterate, const std::vector<double>& solution);
+   void generate_kkt_rhs(const Iterate& current_iterate);
+   void compute_lower_bound_dual_displacements(const Iterate& current_iterate, const std::vector<double>& solution);
+   void compute_upper_bound_dual_displacements(const Iterate& current_iterate, const std::vector<double>& solution);
    Direction generate_direction(const Problem& problem, const Iterate& current_iterate, std::vector<double>& solution_IPM);
+
+   double compute_constraint_violation(const Problem& problem, const Iterate& iterate) const override;
    double compute_KKT_error_scaling(Iterate& current_iterate) const;
    double compute_predicted_reduction(const Direction& direction, double step_length) const;
-   static double constraint_violation(const Problem& problem, Iterate& iterate);
    double compute_central_complementarity_error(Iterate& iterate, double mu, std::vector<Range>& variables_bounds) const;
 };
 
