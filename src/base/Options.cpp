0ee214d76238cdeece43ef0f24209800cbc35f09
Charlie Vanaret <vanaret@zib.de>
2021-08-09 20:12:23 +0200

    Moved the option-related stuff to Options.[c|h]pp

---
+++ src/base/Options.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "Options.hpp"
+#include "Logger.hpp"
+
+Options get_options(const std::string& file_name) {
+   std::ifstream file;
+   file.open(file_name);
+   if (!file) {
+      throw std::invalid_argument("The configuration file was not found");
+   }
+   else {
+      /* register the default values */
+      Options options;
+      std::string key, value;
+      std::string line;
+      while (std::getline(file, line)) {
+         if (!line.empty() && line.find("#") != 0) {
+            std::istringstream iss;
+            iss.str(line);
+            iss >> key >> value;
+            options[key] = value;
+         }
+      }
+      file.close();
+      return options;
+   }
+}
+
+void get_command_options(int argc, char* argv[], Options& options) {
+   /* build the (argument, value) map */
+   int i = 1;
+   while (i < argc - 1) {
+      std::string argument_i = std::string(argv[i]);
+      std::string name = argument_i.substr(1);
+
+      if (argument_i[0] == '-') {
+         // shortcuts for state-of-the-art combinations
+         if (name == "ipopt") {
+            options["mechanism"] = "LS";
+            options["constraint-relaxation"] = "feasibility-restoration";
+            options["strategy"] = "filter";
+            options["subproblem"] = "IPM";
+            i++;
+         }
+         else if (name == "filtersqp") {
+            options["mechanism"] = "TR";
+            options["constraint-relaxation"] = "feasibility-restoration";
+            options["strategy"] = "filter";
+            options["subproblem"] = "SQP";
+            i++;
+         }
+         // standard options
+         else if (i < argc - 1) {
+            std::string value_i = std::string(argv[i + 1]);
+            std::cout << "(" << argument_i << ", " << value_i << ")\n";
+            options[name] = value_i;
+            i += 2;
+         }
+      }
+      else {
+         std::cout << "Argument " << argument_i << " was ignored\n";
+         i++;
+      }
+   }
+}
+
+void print_options(const Options& options) {
+   for (const auto& [key, value]: options) {
+      std::cout << "Option " << key << " = " << value << "\n";
+   }
+}
+
+void set_logger(const Options& options) {
+   try {
+      const std::string logger_level = options.at("logger");
+
+      if (logger_level == "ERROR") {
+         Logger::logger_level = ERROR;
+      }
+      else if (logger_level == "WARNING") {
+         Logger::logger_level = WARNING;
+      }
+      else if (logger_level == "INFO") {
+         Logger::logger_level = INFO;
+      }
+      else if (logger_level == "DEBUG") {
+         Logger::logger_level = DEBUG;
+      }
+   }
+   catch (std::out_of_range&) {
+   }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@
 #include "FeasibilityRestoration.hpp"
 #include "Uno.hpp"
 #include "Logger.hpp"
+#include "Options.hpp"
 
 // new overload to track heap allocations
 size_t total_allocations = 0;
@@ -24,7 +25,7 @@ void* operator new(size_t size) {
    return malloc(size);
 }
 
-void run_uno(const std::string& problem_name, const std::map<std::string, std::string>& options) {
+void run_uno(const std::string& problem_name, const Options& options) {
    // TODO: use a factory
    // AMPL model (Hessian with a Fortran indexing starting at 1)
    auto problem = std::make_unique<AMPLModel>(problem_name, 1);
@@ -63,80 +64,15 @@ void run_uno(const std::string& problem_name, const std::map<std::string, std::s
    std::cout << "Heap allocations: " << total_allocations << "\n";
 }
 
-std::map<std::string, std::string> get_command_options(int argc, char* argv[], std::map<std::string, std::string>& options) {
-   /* build the (argument, value) map */
-   int i = 1;
-   while (i < argc - 1) {
-      std::string argument_i = std::string(argv[i]);
-
-      if (argument_i[0] == '-' && i < argc - 1) {
-         std::string value_i = std::string(argv[i + 1]);
-         std::cout << "(" << argument_i << ", " << value_i << ")\n";
-         options[argument_i.substr(1)] = value_i;
-         i += 2;
-      }
-      else {
-         std::cout << "Argument " << argument_i << " was ignored\n";
-         i++;
-      }
-   }
-   return options;
-}
-
 Level Logger::logger_level = DEBUG;
-
-void set_logger(std::map<std::string, std::string> options) {
-   try {
-      const std::string logger_level = options.at("logger");
-
-      if (logger_level == "ERROR") {
-         Logger::logger_level = ERROR;
-      }
-      else if (logger_level == "WARNING") {
-         Logger::logger_level = WARNING;
-      }
-      else if (logger_level == "INFO") {
-         Logger::logger_level = INFO;
-      }
-      else if (logger_level == "DEBUG") {
-         Logger::logger_level = DEBUG;
-      }
-   }
-   catch (std::out_of_range&) {
-   }
-}
-
-std::map<std::string, std::string> get_options(const std::string& file_name) {
-   std::ifstream file;
-   file.open(file_name);
-   if (!file) {
-      throw std::invalid_argument("The configuration file was not found");
-   }
-   else {
-      /* register the default values */
-      std::map<std::string, std::string> options;
-      std::string key, value;
-      std::string line;
-      while (std::getline(file, line)) {
-         if (!line.empty() && line.find("#") != 0) {
-            std::istringstream iss;
-            iss.str(line);
-            iss >> key >> value;
-            std::cout << "Option " << key << " = " << value << "\n";
-            options[key] = value;
-         }
-      }
-      file.close();
-      return options;
-   }
-}
 
 int main(int argc, char* argv[]) {
    if (1 < argc) {
       /* get the options */
-      std::map<std::string, std::string> options = get_options("uno.cfg");
+      Options options = get_options("uno.cfg");
       /* get the command line options */
       get_command_options(argc, argv, options);
+      print_options(options);
       set_logger(options);
 
       if (std::string(argv[1]) == "-v") {
