204f02472202b3baeda7b67356a4c5292f8fff85
cvanaret <vanaret@math.tu-berlin.de>
2021-08-10 14:26:59 +0200

    LinearSolver: RHS is const

--- src/solvers/linear/LinearSolver.hpp
+++ src/solvers/linear/LinearSolver.hpp
@@ -11,7 +11,7 @@ public:
     virtual void factorize(COOMatrix& matrix) = 0;
     virtual void do_symbolic_factorization(COOMatrix& matrix) = 0;
     virtual void do_numerical_factorization(COOMatrix& matrix) = 0;
-    virtual std::vector<double> solve(const COOMatrix& matrix, std::vector<double>& rhs) = 0;
+    virtual std::vector<double> solve(const COOMatrix& matrix, const std::vector<double>& rhs) = 0;
 
     [[nodiscard]] virtual std::tuple<int, int, int> get_inertia() const = 0;
     [[nodiscard]] virtual size_t number_negative_eigenvalues() const = 0;
