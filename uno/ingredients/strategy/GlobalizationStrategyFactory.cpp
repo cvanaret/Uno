e845fa389139480268c9a46af0ef6c4aa68c6bcd
cvanaret <vanaret@math.tu-berlin.de>
2021-08-23 18:38:41 +0200

    Filter strategy: gave the options a better name (decrease_fraction)

--- uno/ingredients/strategy/GlobalizationStrategyFactory.cpp
+++ uno/ingredients/strategy/GlobalizationStrategyFactory.cpp
@@ -2,19 +2,20 @@
 #include "l1MeritFunction.hpp"
 #include "FilterStrategy.hpp"
 
-std::unique_ptr<GlobalizationStrategy> GlobalizationStrategyFactory::create(const std::string& strategy_type, const Options& options) {
-    if (strategy_type == "l1-penalty") {
-        return std::make_unique<l1MeritFunction>();
-    }
-    else if (strategy_type == "filter" || strategy_type == "nonmonotone-filter") {
-        double Sigma = stod(options.at("Sigma"));
-        double Delta = stod(options.at("Delta"));
-        double ubd = stod(options.at("ubd"));
-        double fact = stod(options.at("fact"));
-        FilterStrategyParameters strategy_parameters = {Sigma, Delta, ubd, fact};
-        return std::make_unique<FilterStrategy>(strategy_parameters, options);
-    }
-    else {
-        throw std::invalid_argument("GlobalizationStrategy type " + strategy_type + " does not exist");
-    }
+std::unique_ptr <GlobalizationStrategy> GlobalizationStrategyFactory::create(const std::string& strategy_type, const Options& options) {
+   if (strategy_type == "l1-penalty") {
+      const double decrease_fraction = stod(options.at("decrease_fraction"));
+      return std::make_unique<l1MeritFunction>(decrease_fraction);
+   }
+   else if (strategy_type == "filter" || strategy_type == "nonmonotone-filter") {
+      const double decrease_fraction = stod(options.at("decrease_fraction"));
+      const double delta = stod(options.at("Delta"));
+      const double ubd = stod(options.at("ubd"));
+      const double fact = stod(options.at("fact"));
+      FilterStrategyParameters strategy_parameters = {decrease_fraction, delta, ubd, fact};
+      return std::make_unique<FilterStrategy>(strategy_parameters, options);
+   }
+   else {
+      throw std::invalid_argument("GlobalizationStrategy type " + strategy_type + " does not exist");
+   }
 }
--- uno/ingredients/subproblem/HessianEvaluation.hpp
+++ uno/ingredients/subproblem/HessianEvaluation.hpp
@@ -8,11 +8,10 @@
 #include "solvers/linear/LinearSolverFactory.hpp"
 
 // virtual (abstract) class
-template <class SparseSymmetricMatrix>
+template<class SparseSymmetricMatrix>
 class HessianEvaluation {
 public:
-   explicit HessianEvaluation(size_t dimension, size_t hessian_maximum_number_nonzeros): hessian(dimension, hessian_maximum_number_nonzeros) {
-   }
+   explicit HessianEvaluation(size_t dimension, size_t hessian_maximum_number_nonzeros);
    virtual ~HessianEvaluation() = default;
 
    SparseSymmetricMatrix hessian;
@@ -21,50 +20,61 @@ public:
    virtual void compute(const Problem& problem, const std::vector<double>& primal_variables, double objective_multiplier,
          const std::vector<double>& constraint_multipliers) = 0;
 
-   SparseSymmetricMatrix modify_inertia(SparseSymmetricMatrix& matrix, LinearSolver<SparseSymmetricMatrix>& linear_solver) {
-      const double beta = 1e-4;
-
-      // Nocedal and Wright, p51
-      double smallest_diagonal_entry = matrix.smallest_diagonal_entry();
-      DEBUG << "The minimal diagonal entry of the Hessian is " << matrix.smallest_diagonal_entry() << "\n";
-
-      double inertia = 0.;
-      double previous_inertia = 0.;
-      if (smallest_diagonal_entry <= 0.) {
-         inertia = beta - smallest_diagonal_entry;
-      }
-
-      if (0. < inertia) {
-         matrix.add_identity_multiple(inertia - previous_inertia);
-      }
-
-      DEBUG << "Testing factorization with inertia term " << inertia << "\n";
-      linear_solver.do_symbolic_factorization(matrix);
-      linear_solver.do_numerical_factorization(matrix);
-
-      bool good_inertia = false;
-      while (!good_inertia) {
-         DEBUG << linear_solver.number_negative_eigenvalues() << " negative eigenvalues\n";
-         if (!linear_solver.matrix_is_singular() && linear_solver.number_negative_eigenvalues() == 0) {
-            good_inertia = true;
-            DEBUG << "Factorization was a success with inertia " << inertia << "\n";
-         }
-         else {
-            previous_inertia = inertia;
-            inertia = (inertia == 0.) ? beta : 2*inertia;
-            matrix.add_identity_multiple(inertia - previous_inertia);
-            DEBUG << "Testing factorization with inertia term " << inertia << "\n";
-            linear_solver.do_numerical_factorization(matrix);
-         }
-      }
-      return matrix;
-   }
+   SparseSymmetricMatrix modify_inertia(SparseSymmetricMatrix& matrix, LinearSolver <SparseSymmetricMatrix>& linear_solver);
 };
 
-template <class SparseSymmetricMatrix>
+template<class SparseSymmetricMatrix>
+inline HessianEvaluation<SparseSymmetricMatrix>::HessianEvaluation(size_t dimension, size_t hessian_maximum_number_nonzeros):
+      hessian(dimension, hessian_maximum_number_nonzeros) {
+}
+
+template<class SparseSymmetricMatrix>
+inline SparseSymmetricMatrix HessianEvaluation<SparseSymmetricMatrix>::modify_inertia(SparseSymmetricMatrix& matrix,
+      LinearSolver <SparseSymmetricMatrix>& linear_solver) {
+   const double beta = 1e-4;
+
+   // Nocedal and Wright, p51
+   double smallest_diagonal_entry = matrix.smallest_diagonal_entry();
+   DEBUG << "The minimal diagonal entry of the Hessian is " << matrix.smallest_diagonal_entry() << "\n";
+
+   double inertia = 0.;
+   double previous_inertia = 0.;
+   if (smallest_diagonal_entry <= 0.) {
+      inertia = beta - smallest_diagonal_entry;
+   }
+
+   if (0. < inertia) {
+      matrix.add_identity_multiple(inertia - previous_inertia);
+   }
+
+   DEBUG << "Testing factorization with inertia term " << inertia << "\n";
+   linear_solver.do_symbolic_factorization(matrix);
+   linear_solver.do_numerical_factorization(matrix);
+
+   bool good_inertia = false;
+   while (!good_inertia) {
+      DEBUG << linear_solver.number_negative_eigenvalues() << " negative eigenvalues\n";
+      if (!linear_solver.matrix_is_singular() && linear_solver.number_negative_eigenvalues() == 0) {
+         good_inertia = true;
+         DEBUG << "Factorization was a success with inertia " << inertia << "\n";
+      }
+      else {
+         previous_inertia = inertia;
+         inertia = (inertia == 0.) ? beta : 2 * inertia;
+         matrix.add_identity_multiple(inertia - previous_inertia);
+         DEBUG << "Testing factorization with inertia term " << inertia << "\n";
+         linear_solver.do_numerical_factorization(matrix);
+      }
+   }
+   return matrix;
+}
+
+// Exact Hessian
+
+template<class SparseSymmetricMatrix>
 class ExactHessianEvaluation : public HessianEvaluation<SparseSymmetricMatrix> {
 public:
-   explicit ExactHessianEvaluation(size_t dimension, size_t hessian_maximum_number_nonzeros): HessianEvaluation<SparseSymmetricMatrix>(dimension,
+   explicit ExactHessianEvaluation(size_t dimension, size_t hessian_maximum_number_nonzeros) : HessianEvaluation<SparseSymmetricMatrix>(dimension,
          hessian_maximum_number_nonzeros) {
    }
 
@@ -101,17 +111,17 @@ public:
 //   std::unique_ptr<LinearSolver<COOSymmetricMatrix> > linear_solver; /*!< Solver that computes the inertia */
 //};
 
-template <class SparseSymmetricMatrix>
+template<class SparseSymmetricMatrix>
 class HessianEvaluationFactory {
 public:
-   static std::unique_ptr<HessianEvaluation<SparseSymmetricMatrix> > create(const std::string& hessian_evaluation_method, size_t dimension,
+   static std::unique_ptr <HessianEvaluation<SparseSymmetricMatrix>> create(const std::string& hessian_evaluation_method, size_t dimension,
          size_t hessian_maximum_number_nonzeros, bool convexify) {
       if (hessian_evaluation_method == "exact") {
          if (convexify) {
-         //   return std::make_unique<ConvexifiedExactHessianEvaluation<SparseSymmetricMatrix> >(dimension, hessian_maximum_number_nonzeros, "MA57");
+            //   return std::make_unique<ConvexifiedExactHessianEvaluation<SparseSymmetricMatrix> >(dimension, hessian_maximum_number_nonzeros, "MA57");
          }
          //else {
-            return std::make_unique<ExactHessianEvaluation<SparseSymmetricMatrix> >(dimension, hessian_maximum_number_nonzeros);
+         return std::make_unique<ExactHessianEvaluation<SparseSymmetricMatrix> >(dimension, hessian_maximum_number_nonzeros);
          //}
       }
       else {
