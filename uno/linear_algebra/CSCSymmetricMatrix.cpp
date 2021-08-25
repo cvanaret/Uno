262c760f44fdcf44a849c29594c7b2e8bac8560b
Charlie Vanaret <vanaret@zib.de>
2021-08-25 17:41:18 +0200

    Implemented CSC matrices with padding + elegant implementation of M + k*I using padding

--- uno/linear_algebra/CSCSymmetricMatrix.cpp
+++ uno/linear_algebra/CSCSymmetricMatrix.cpp
@@ -11,76 +11,96 @@
 
 // matrix and row_index have nnz elements
 // column_start has dimension+1 elements
+// padding provides padding_size slots at the end of each column
 
-CSCSymmetricMatrix::CSCSymmetricMatrix(int dimension, size_t capacity) : SymmetricMatrix(dimension, capacity),
-      matrix(capacity), column_start(dimension + 1), row_index(capacity) {
+CSCSymmetricMatrix::CSCSymmetricMatrix(int dimension, size_t capacity, size_t padding_size) : SymmetricMatrix(dimension, capacity),
+matrix(capacity + dimension*padding_size), column_start(dimension + 1), row_index(capacity + dimension*padding_size),
+padding_size(padding_size), remaining_column_padding(dimension, padding_size) {
 }
 
 CSCSymmetricMatrix::CSCSymmetricMatrix(std::vector<double> matrix, const std::vector<int>& column_start, std::vector<int> row_number, int
 capacity) : SymmetricMatrix((int) column_start.size() - 1, capacity), matrix(std::move(matrix)), column_start(column_start), row_index(std::move
-(row_number)) {
+(row_number)), padding_size(0), remaining_column_padding(dimension, 0) {
+   assert(false && "padding should be fixed");
 }
 
 // generic iterator
 void CSCSymmetricMatrix::for_each(const std::function<void (int, int, double)>& f) const {
+   size_t overall_padding_size = 0;
    for (int j = 0; j < this->dimension; j++) {
-      for (int k = this->column_start[j]; k < this->column_start[j + 1]; k++) {
+      // work on the active elements (ignore the padding slots)
+      for (int k = this->column_start[j] + overall_padding_size; k < this->column_start[j + 1] + overall_padding_size; k++) {
          int i = this->row_index[k];
          f(i, j, this->matrix[k]);
       }
+      overall_padding_size += this->remaining_column_padding[j];
    }
 }
 
-void CSCSymmetricMatrix::insert(double /*term*/, int /*row_index*/, int /*column_index*/) {
-   assert(false && "CSCSymmetricMatrix::insert is not implemented");
+void CSCSymmetricMatrix::insert(double term, int row_index, int column_index) {
+   assert(this->current_insertion_index <= this->matrix.capacity() && "The matrix does not have sufficient capacity");
+   assert(column_index == this->current_column && "The previous columns should be finalized");
+   this->matrix[this->current_insertion_index] = term;
+   this->row_index[this->current_insertion_index] = row_index;
+   this->column_start[column_index + 1]++;
+   this->current_insertion_index++;
+   this->number_nonzeros++;
 }
 
-void CSCSymmetricMatrix::add_identity_multiple(double multiple) {
-   /* initialize the damped matrix */
-   std::vector<double> damped_matrix;
-   std::vector<int> damped_column_start;
-   std::vector<int> damped_row_number;
-   damped_matrix.reserve(this->capacity);
-   damped_row_number.reserve(this->capacity);
-   damped_column_start.reserve(this->dimension);
+void CSCSymmetricMatrix::finalize(size_t column_index) {
+   assert(column_index == this->current_column && "You are not finalizing the current column");
+   assert(column_index < this->dimension && "The dimension of the matrix was exceeded");
+   // add padding
+   this->current_insertion_index += this->padding_size;
 
-   int current_number_nonzeros = 0;
-   damped_column_start.push_back(0);
+   if (column_index < this->dimension - 1) {
+      this->column_start[column_index + 2] = this->column_start[column_index + 1];
+      this->current_column++;
+   }
+}
 
-   /* go through the columns */
+void CSCSymmetricMatrix::add_identity_multiple(double factor) {
+   size_t overall_padding_size = 0;
    for (int j = 0; j < this->dimension; j++) {
-      bool diagonal_term_updated = false;
-
-      for (int k = this->column_start[j]; k < this->column_start[j + 1]; k++) {
-         /* compute row number */
-         int i = this->row_index[k];
-
-         if (i == j) { /* update diagonal term */
-            damped_matrix.push_back(this->matrix[k] + multiple);
-            diagonal_term_updated = true;
+      // check if the last element in the column is diagonal
+      const size_t number_elements = this->column_start[j+1] - this->column_start[j];
+      bool insert_new_element = false;
+      if (0 < number_elements) {
+         const size_t last_element_index = this->column_start[j+1] - 1 + overall_padding_size;
+         const int i = this->row_index[last_element_index];
+         if (i == j) {
+            // the diagonal element already exists: simply add factor
+            this->matrix[last_element_index] += factor;
          }
-         else if (j < i && !diagonal_term_updated) { /* we passed the diagonal (j, j) */
-            damped_matrix.push_back(multiple);
-            damped_matrix.push_back(this->matrix[k]);
-            damped_row_number.push_back(j); // diagonal term
-            current_number_nonzeros++;
-            diagonal_term_updated = true;
+         else { // column terminates at a non-diagonal element
+            insert_new_element = true;
          }
-         else { /* keep off-diagonal term */
-            damped_matrix.push_back(this->matrix[k]);
-         }
-         damped_row_number.push_back(i);
-         current_number_nonzeros++;
       }
-      /* add diagonal term in column j if not present */
-      if (!diagonal_term_updated) {
-         damped_matrix.push_back(multiple);
-         damped_row_number.push_back(j);
-         current_number_nonzeros++;
+      else { // empty column
+         insert_new_element = true;
       }
-      damped_column_start.push_back(current_number_nonzeros);
+
+      // insert a new element: there must be at least one padding slot
+      if (insert_new_element) {
+         assert(0 < this->remaining_column_padding[j] && "Padding is not sufficient to insert a new element");
+         const size_t last_element_index = this->column_start[j+1] - 1 + overall_padding_size;
+         // insert the new diagonal element at the first padding slot
+         this->matrix[last_element_index+1] = factor;
+         this->row_index[last_element_index+1] = j;
+         // shift the next column starts
+         for (int k = j + 1; k < this->dimension + 1; k++) {
+            this->column_start[k]++;
+         }
+         this->number_nonzeros++;
+         this->current_insertion_index++;
+         this->remaining_column_padding[j]--;
+      }
+      overall_padding_size += this->remaining_column_padding[j];
    }
-   *this = {damped_matrix, damped_column_start, damped_row_number, (int) this->capacity};
+}
+
+void CSCSymmetricMatrix::force_explicit_diagonal_elements() {
+   this->add_identity_multiple(0.);
 }
 
 COOSymmetricMatrix CSCSymmetricMatrix::to_COO() {
@@ -99,28 +119,29 @@ COOSymmetricMatrix CSCSymmetricMatrix::to_COO() {
 }
 
 CSCSymmetricMatrix CSCSymmetricMatrix::identity(int dimension) {
-   /* initialize the identity matrix */
-   std::vector<double> matrix(dimension);
-   std::vector<int> column_start(dimension + 1);
-   std::vector<int> row_index(dimension);
-
-   column_start[0] = 0;
+   // no padding
+   CSCSymmetricMatrix identity(dimension, dimension, 0);
    for (int i = 0; i < dimension; i++) {
-      matrix[i] = 1.;
-      row_index[i] = i;
-      column_start[i + 1] = i + 1;
+      identity.insert(1., i, i);
+      identity.finalize(i);
    }
-   return {matrix, column_start, row_index, dimension};
+   return identity;
 }
 
 std::ostream& operator<<(std::ostream& stream, CSCSymmetricMatrix& matrix) {
    stream << matrix.number_nonzeros << " non zeros\n";
-   stream << "W = ";
-   print_vector(stream, matrix.matrix, '\n', 0, matrix.number_nonzeros);
+   stream << "W =";
+   matrix.for_each([&](size_t /*i*/, size_t /*j*/, double entry) {
+      stream << " " << entry;
+   });
+   stream << "\n";
    stream << "with column start: ";
    print_vector(stream, matrix.column_start, '\n', 0, matrix.dimension + 1);
-   stream << "and row index: ";
-   print_vector(stream, matrix.row_index, '\n', 0, matrix.number_nonzeros);
+   stream << "and row index:";
+   matrix.for_each([&](size_t i, size_t /*j*/, double /*entry*/) {
+      stream << " " << i;
+   });
+   stream << "\n";
    return stream;
 }
 
--- uno/linear_algebra/CSCSymmetricMatrix.hpp
+++ uno/linear_algebra/CSCSymmetricMatrix.hpp
@@ -15,19 +15,28 @@ public:
    std::vector<double> matrix;
    std::vector<int> column_start;
    std::vector<int> row_index;
+   const size_t padding_size;
 
-   CSCSymmetricMatrix(int dimension, size_t maximum_number_nonzeros);
+   CSCSymmetricMatrix(int dimension, size_t maximum_number_nonzeros, size_t padding_size = 0);
    CSCSymmetricMatrix(std::vector<double> matrix, const std::vector<int>& column_start, std::vector<int> row_number, int capacity);
 
    void for_each(const std::function<void (int, int, double)>& f) const override;
    void insert(double term, int row_index, int column_index) override;
+   void finalize(size_t column_index);
+   void force_explicit_diagonal_elements();
    static CSCSymmetricMatrix identity(int dimension);
 
-   void add_identity_multiple(double multiple) override;
+   void add_identity_multiple(double factor) override;
    COOSymmetricMatrix to_COO();
 
    friend std::ostream& operator<<(std::ostream& stream, CSCSymmetricMatrix& matrix);
    friend std::ostream& operator<<(std::ostream& stream, const CSCSymmetricMatrix& matrix);
+
+protected:
+   // when elements are inserted one by one, keep track of the current column
+   size_t current_column{0};
+   size_t current_insertion_index{0};
+   std::vector<size_t> remaining_column_padding;
 };
 
 #endif // CSCSYMMETRICMATRIX_H
