7ac2bdcf4813d7fd0059583dd2e39e1229c5fcec
Charlie Vanaret <vanaret@zib.de>
2021-09-07 09:55:18 +0200

    Reuse current iterate when d = 0

--- uno/ingredients/mechanism/GlobalizationMechanism.cpp
+++ uno/ingredients/mechanism/GlobalizationMechanism.cpp
@@ -5,15 +5,24 @@ GlobalizationMechanism::GlobalizationMechanism(ConstraintRelaxationStrategy& con
       max_iterations(max_iterations) {
 }
 
-Iterate GlobalizationMechanism::assemble_trial_iterate(const Iterate& current_iterate, Direction& direction, double step_length) {
-   // TODO do not reevaluate if d = 0
-   Iterate trial_iterate(direction.x.size(), direction.multipliers.constraints.size());
-   add_vectors(current_iterate.x, direction.x, step_length, trial_iterate.x);
-   add_vectors(current_iterate.multipliers.constraints, direction.multipliers.constraints, step_length, trial_iterate.multipliers.constraints);
-   copy_from(trial_iterate.multipliers.lower_bounds, direction.multipliers.lower_bounds);
-   copy_from(trial_iterate.multipliers.upper_bounds, direction.multipliers.upper_bounds);
-   trial_iterate.multipliers.objective = direction.objective_multiplier;
-   return trial_iterate;
+Iterate GlobalizationMechanism::assemble_trial_iterate(Iterate& current_iterate, Direction& direction, double step_length) {
+   if (direction.norm == 0.) {
+      add_vectors(current_iterate.multipliers.constraints, direction.multipliers.constraints, step_length, current_iterate.multipliers.constraints);
+      copy_from(current_iterate.multipliers.lower_bounds, direction.multipliers.lower_bounds);
+      copy_from(current_iterate.multipliers.upper_bounds, direction.multipliers.upper_bounds);
+      current_iterate.multipliers.objective = direction.objective_multiplier;
+      current_iterate.progress = {0., 0.};
+      return current_iterate;
+   }
+   else {
+      Iterate trial_iterate(direction.x.size(), direction.multipliers.constraints.size());
+      add_vectors(current_iterate.x, direction.x, step_length, trial_iterate.x);
+      add_vectors(current_iterate.multipliers.constraints, direction.multipliers.constraints, step_length, trial_iterate.multipliers.constraints);
+      copy_from(trial_iterate.multipliers.lower_bounds, direction.multipliers.lower_bounds);
+      copy_from(trial_iterate.multipliers.upper_bounds, direction.multipliers.upper_bounds);
+      trial_iterate.multipliers.objective = direction.objective_multiplier;
+      return trial_iterate;
+   }
 }
 
 int GlobalizationMechanism::get_hessian_evaluation_count() const {
