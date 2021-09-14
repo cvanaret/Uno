edd845236c8d39ac139ac23cfed40bd2f84ce4c8
Charlie Vanaret <vanaret@zib.de>
2021-09-14 12:04:50 +0200

    Added const to GlobalizationStrategy parameters

--- uno/ingredients/strategy/l1MeritFunction.hpp
+++ uno/ingredients/strategy/l1MeritFunction.hpp
@@ -12,8 +12,8 @@ public:
    explicit l1MeritFunction(double decrease_fraction);
 
    void initialize(Statistics& statistics, const Iterate& first_iterate) override;
-   bool check_acceptance(Statistics& statistics, ProgressMeasures& current_progress, ProgressMeasures& trial_progress, double objective_multiplier,
-         double predicted_reduction) override;
+   bool check_acceptance(Statistics& statistics, const ProgressMeasures& current_progress, const ProgressMeasures& trial_progress, double
+      objective_multiplier, double predicted_reduction) override;
    void reset() override;
    void notify(Iterate& current_iterate) override;
 
