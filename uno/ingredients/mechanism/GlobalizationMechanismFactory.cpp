a344474d2fcea5407835626937036c9df81059a7
cvanaret <vanaret@math.tu-berlin.de>
2021-12-16 12:19:11 +0100

    Renamed TrustRegion to TrustRegionStrategy

--- uno/ingredients/mechanism/GlobalizationMechanismFactory.cpp
+++ uno/ingredients/mechanism/GlobalizationMechanismFactory.cpp
@@ -1,12 +1,12 @@
 #include "GlobalizationMechanismFactory.hpp"
-#include "ingredients/mechanism/TrustRegion.hpp"
+#include "ingredients/mechanism/TrustRegionStrategy.hpp"
 #include "ingredients/mechanism/BacktrackingLineSearch.hpp"
 
 std::unique_ptr<GlobalizationMechanism> GlobalizationMechanismFactory::create(ConstraintRelaxationStrategy& constraint_relaxation_strategy, const
 Options& options) {
    const std::string mechanism_type = options.at("mechanism");
     if (mechanism_type == "TR") {
-        return std::make_unique<TrustRegion>(constraint_relaxation_strategy, options);
+        return std::make_unique<TrustRegionStrategy>(constraint_relaxation_strategy, options);
     }
     else if (mechanism_type == "LS") {
         return std::make_unique<BacktrackingLineSearch>(constraint_relaxation_strategy, options);
