1b9176d3e28e03b50554dfbb2b974aaa576e5519
cvanaret <vanaret@math.tu-berlin.de>
2022-01-06 15:22:46 +0100

    Changed the command line value l1-penalty to l1-merit

--- uno/ingredients/strategy/GlobalizationStrategyFactory.cpp
+++ uno/ingredients/strategy/GlobalizationStrategyFactory.cpp
@@ -3,7 +3,7 @@
 #include "FilterStrategy.hpp"
 
 std::unique_ptr <GlobalizationStrategy> GlobalizationStrategyFactory::create(const std::string& strategy_type, const Options& options) {
-   if (strategy_type == "l1-penalty") {
+   if (strategy_type == "l1-merit") {
       return std::make_unique<l1MeritFunction>(options);
    }
    else if (strategy_type == "filter" || strategy_type == "nonmonotone-filter") {
