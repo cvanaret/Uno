32a605410b0b6bb2934df2c50c9f6f9197b582ba
Charlie Vanaret <vanaret@zib.de>
2022-04-14 10:42:08 +0200

    Removed subproblem_evaluations from QP and LP

--- uno/ingredients/subproblem/LPSubproblem.cpp
+++ uno/ingredients/subproblem/LPSubproblem.cpp
@@ -3,21 +3,36 @@
 
 LPSubproblem::LPSubproblem(const Problem& problem, size_t max_number_variables, const Options& options) :
       ActiveSetSubproblem(max_number_variables, problem.number_constraints, NO_SOC, false, norm_from_string(options.at("residual_norm"))),
-      solver(LPSolverFactory::create(max_number_variables, problem.number_constraints, options.at("LP_solver"))) {
+      solver(LPSolverFactory::create(max_number_variables, problem.number_constraints, options.at("LP_solver"))),
+      objective_gradient(max_number_variables),
+      constraints(problem.number_constraints),
+      constraint_jacobian(problem.number_constraints) {
+   for (auto& constraint_gradient: this->constraint_jacobian) {
+      constraint_gradient.reserve(max_number_variables);
+   }
 }
 
 void LPSubproblem::build_objective_model(const Problem& problem, Iterate& current_iterate, double /*objective_multiplier*/) {
-   // objective gradient
-   current_iterate.evaluate_objective_gradient(problem);
+
 }
 
 void LPSubproblem::build_constraint_model(const Problem& problem, Iterate& current_iterate) {
-   // compute first- and second-order information
-   current_iterate.evaluate_constraints(problem);
-   current_iterate.evaluate_constraint_jacobian(problem);
+
 }
 
 Direction LPSubproblem::solve(Statistics& /*statistics*/, const Problem& problem, Iterate& current_iterate) {
+   // objective gradient
+   current_iterate.evaluate_objective_gradient(problem);
+   this->objective_gradient = current_iterate.problem_evaluations.objective_gradient;
+
+   // constraints
+   current_iterate.evaluate_constraints(problem);
+   this->constraints = current_iterate.problem_evaluations.constraints;
+
+   // constraint Jacobian
+   current_iterate.evaluate_constraint_jacobian(problem);
+   this->constraint_jacobian = current_iterate.problem_evaluations.constraint_jacobian;
+
    // bounds of the variable displacements
    this->set_variable_displacement_bounds(problem, current_iterate);
 
@@ -26,8 +41,7 @@ Direction LPSubproblem::solve(Statistics& /*statistics*/, const Problem& problem
 
    // solve the LP
    Direction direction = this->solver->solve_LP(problem.number_variables, problem.number_constraints, this->variable_displacement_bounds,
-         this->linearized_constraint_bounds, current_iterate.subproblem_evaluations.objective_gradient,
-         current_iterate.problem_evaluations.constraint_jacobian, this->initial_point);
+         this->linearized_constraint_bounds, this->objective_gradient, this->constraint_jacobian, this->initial_point);
    ActiveSetSubproblem::compute_dual_displacements(problem, current_iterate, direction);
    this->number_subproblems_solved++;
    return direction;
--- uno/ingredients/subproblem/QPSubproblem.cpp
+++ uno/ingredients/subproblem/QPSubproblem.cpp
@@ -8,54 +8,62 @@ QPSubproblem::QPSubproblem(const Problem& problem, size_t max_number_variables, 
             problem.get_hessian_maximum_number_nonzeros()
             + max_number_variables, /* regularization */
             true)),
+      proximal_coefficient(stod(options.at("proximal_coefficient"))),
       // if no trust region is used, the problem should be convexified to guarantee boundedness + descent direction
       hessian_model(HessianModelFactory::create(options.at("hessian_model"), max_number_variables,
             problem.get_hessian_maximum_number_nonzeros() + max_number_variables, options.at("mechanism") != "TR", options)),
-      proximal_coefficient(stod(options.at("proximal_coefficient"))) {
+      objective_gradient(max_number_variables),
+      constraints(problem.number_constraints),
+      constraint_jacobian(problem.number_constraints) {
+   for (auto& constraint_gradient: this->constraint_jacobian) {
+      constraint_gradient.reserve(max_number_variables);
+   }
 }
 
 void QPSubproblem::build_objective_model(const Problem& problem, Iterate& current_iterate, double objective_multiplier) {
+
+}
+
+void QPSubproblem::build_constraint_model(const Problem& problem, Iterate& current_iterate) {
+
+}
+
+Direction QPSubproblem::solve(Statistics& /*statistics*/, const Problem& problem, Iterate& current_iterate) {
    // Hessian
-   this->hessian_model->evaluate(problem, current_iterate.x, objective_multiplier, current_iterate.multipliers.constraints);
+   this->hessian_model->evaluate(problem, current_iterate.x, problem.objective_sign, current_iterate.multipliers.constraints);
    this->hessian_model->adjust_number_variables(problem.number_variables);
 
    // objective gradient
    current_iterate.evaluate_objective_gradient(problem);
-   current_iterate.subproblem_evaluations.objective_gradient = current_iterate.problem_evaluations.objective_gradient;
-}
+   this->objective_gradient = current_iterate.problem_evaluations.objective_gradient;
 
-void QPSubproblem::build_constraint_model(const Problem& problem, Iterate& current_iterate) {
    // constraints
    current_iterate.evaluate_constraints(problem);
+   this->constraints = current_iterate.problem_evaluations.constraints;
 
    // constraint Jacobian
    current_iterate.evaluate_constraint_jacobian(problem);
+   this->constraint_jacobian = current_iterate.problem_evaluations.constraint_jacobian;
 
-   current_iterate.subproblem_evaluations.constraints = current_iterate.problem_evaluations.constraints;
-   current_iterate.subproblem_evaluations.constraint_jacobian = current_iterate.problem_evaluations.constraint_jacobian;
-}
-
-Direction QPSubproblem::solve(Statistics& /*statistics*/, const Problem& problem, Iterate& current_iterate) {
    // bounds of the variable displacements
    this->set_variable_displacement_bounds(problem, current_iterate);
 
    // bounds of the linearized constraints
-   this->set_linearized_constraint_bounds(problem, current_iterate.subproblem_evaluations.constraints);
+   this->set_linearized_constraint_bounds(problem, this->constraints);
 
    // compute QP direction
    Direction direction = this->solver->solve_QP(problem.number_variables, problem.number_constraints, this->variable_displacement_bounds,
-         this->linearized_constraint_bounds, current_iterate.subproblem_evaluations.objective_gradient,
-         current_iterate.subproblem_evaluations.constraint_jacobian, *this->hessian_model->hessian, this->initial_point);
+         this->linearized_constraint_bounds, this->objective_gradient, this->constraint_jacobian, *this->hessian_model->hessian, this->initial_point);
    ActiveSetSubproblem::compute_dual_displacements(problem, current_iterate, direction);
    this->number_subproblems_solved++;
    return direction;
 }
 
-PredictedReductionModel QPSubproblem::generate_predicted_reduction_model(const Problem& problem, const Iterate& current_iterate,
+PredictedReductionModel QPSubproblem::generate_predicted_reduction_model(const Problem& problem, const Iterate& /*current_iterate*/,
       const Direction& direction) const {
    return PredictedReductionModel(-direction.objective, [&]() { // capture this and direction by reference
       // precompute expensive quantities
-      const double linear_term = dot(direction.x, current_iterate.subproblem_evaluations.objective_gradient);
+      const double linear_term = dot(direction.x, this->objective_gradient);
       const double quadratic_term = this->hessian_model->hessian->quadratic_product(direction.x, direction.x, problem.number_variables) / 2.;
       // return a function of the step length that cheaply assembles the predicted reduction
       return [=](double step_length) { // capture the expensive quantities by value
