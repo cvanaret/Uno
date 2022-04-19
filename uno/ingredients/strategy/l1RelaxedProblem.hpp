4b5e2c4835ef7c2b69f49f9f94d45b2cbfdc258d
Charlie Vanaret <vanaret@zib.de>
2022-04-19 17:32:29 +0200

    Minor cleanup

--- uno/ingredients/strategy/l1RelaxedProblem.hpp
+++ uno/ingredients/strategy/l1RelaxedProblem.hpp
@@ -28,15 +28,13 @@ public:
    [[nodiscard]] double compute_linearized_constraint_violation(const std::vector<double>& x) const;
    [[nodiscard]] double compute_linearized_constraint_violation(const std::vector<double>& x, const std::vector<double>& dx) const;
 
+   // parameterization
    void set_objective_multiplier(double new_objective_multiplier);
-
    void set_proximal_coefficient(double new_proximal_coefficient);
    void set_proximal_reference_point(const std::vector<double>& new_proximal_reference_point);
 
    // void set_elastic_variables(Iterate& iterate) const;
    void reset_elastic_variables(Iterate& iterate, double value = 0.) const;
-   void add_elastics_to_constraints(const std::vector<double>& x, std::vector<double>& constraints) const;
-   void add_elastics_to_constraint_jacobian(std::vector<SparseVector<double>>& constraint_jacobian) const;
 
 protected:
    double objective_multiplier;
@@ -49,13 +47,15 @@ protected:
    std::vector<double> proximal_reference_point;
 
    [[nodiscard]] static size_t count_elastic_variables(const Model& model);
-   static void generate_elastic_variables(const Model& model, ElasticVariables& elastic_variables, size_t number_variables);
+   void generate_elastic_variables();
    [[nodiscard]] double get_proximal_weight(size_t i) const;
+   void add_elastics_to_constraints(const std::vector<double>& x, std::vector<double>& constraints) const;
+   void add_elastics_to_constraint_jacobian(std::vector<SparseVector<double>>& constraint_jacobian) const;
 };
 
 inline l1RelaxedProblem::l1RelaxedProblem(const Model& model, double objective_multiplier,
       double elastic_objective_coefficient, bool use_proximal_term):
-      NonlinearProblem(model, model.number_variables + l1RelaxedProblem::count_elastic_variables(model), model.number_constraints),
+      NonlinearProblem(model, model.number_variables + this->count_elastic_variables(model), model.number_constraints),
       objective_multiplier(objective_multiplier),
       // elastic variables
       elastic_variables(this->number_constraints),
@@ -71,7 +71,7 @@ inline l1RelaxedProblem::l1RelaxedProblem(const Model& model, double objective_m
    });
 
    // generate elastic variables
-   l1RelaxedProblem::generate_elastic_variables(this->model, this->elastic_variables, this->model.number_variables);
+   this->generate_elastic_variables();
 
    // figure out bounded variables
    for (size_t i: this->model.lower_bounded_variables) {
@@ -283,19 +283,19 @@ inline size_t l1RelaxedProblem::count_elastic_variables(const Model& model) {
    return number_elastic_variables;
 }
 
-inline void l1RelaxedProblem::generate_elastic_variables(const Model& model, ElasticVariables& elastic_variables, size_t number_variables) {
+inline void l1RelaxedProblem::generate_elastic_variables() {
    // generate elastic variables p and n on the fly to relax the constraints
    // if the subproblem uses slack variables, the bounds of the constraints are [0, 0]
-   size_t elastic_index = number_variables;
-   for (size_t j = 0; j < model.number_constraints; j++) {
-      if (is_finite(model.get_constraint_upper_bound(j))) {
+   size_t elastic_index = this->model.number_variables;
+   for (size_t j = 0; j < this->model.number_constraints; j++) {
+      if (is_finite(this->model.get_constraint_upper_bound(j))) {
          // nonnegative variable p that captures the positive part of the constraint violation
-         elastic_variables.positive.insert(j, elastic_index);
+         this->elastic_variables.positive.insert(j, elastic_index);
          elastic_index++;
       }
-      if (is_finite(model.get_constraint_lower_bound(j))) {
+      if (is_finite(this->model.get_constraint_lower_bound(j))) {
          // nonpositive variable n that captures the negative part of the constraint violation
-         elastic_variables.negative.insert(j, elastic_index);
+         this->elastic_variables.negative.insert(j, elastic_index);
          elastic_index++;
       }
    }
