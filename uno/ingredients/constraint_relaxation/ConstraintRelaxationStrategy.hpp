cf70035b8b308b6b33fc1d4acf82d2ec23fbcf53
Charlie Vanaret <vanaret@math.tu-berlin.de>
2022-04-19 17:03:15 +0200

    Moved optimality problem and relaxed problem from ConstraintRelaxationStrategy to FeasibilityRestoration and l1Relaxation

--- uno/ingredients/constraint_relaxation/ConstraintRelaxationStrategy.hpp
+++ uno/ingredients/constraint_relaxation/ConstraintRelaxationStrategy.hpp
@@ -4,21 +4,16 @@
 #include "ingredients/subproblem/Subproblem.hpp"
 #include "ingredients/subproblem/Direction.hpp"
 #include "ingredients/subproblem/PredictedReductionModel.hpp"
-#include "linear_algebra/SparseVector.hpp"
-#include "optimization/Model.hpp"
 #include "optimization/Iterate.hpp"
-#include "ingredients/strategy/OptimalityProblem.hpp"
-#include "ingredients/strategy/l1RelaxedProblem.hpp"
 #include "tools/Statistics.hpp"
 #include "tools/Options.hpp"
 
 class ConstraintRelaxationStrategy {
 public:
-   ConstraintRelaxationStrategy(const Model& model, double objective_multiplier, const Options& options);
+   ConstraintRelaxationStrategy() = default;
    virtual ~ConstraintRelaxationStrategy() = default;
 
    virtual void initialize(Statistics& statistics, Iterate& first_iterate) = 0;
-
    virtual void set_variable_bounds(const Iterate& current_iterate, double trust_region_radius) = 0;
 
    // direction computation
@@ -28,26 +23,19 @@ public:
    virtual Direction compute_second_order_correction(Iterate& trial_iterate) = 0;
 
    // trial iterate acceptance
+   [[nodiscard]] virtual PredictedReductionModel generate_predicted_reduction_model(const Direction& direction) const = 0;
    virtual bool is_acceptable(Statistics& statistics, Iterate& current_iterate, Iterate& trial_iterate, const Direction& direction,
          PredictedReductionModel& predicted_reduction_model, double step_length = 1.) = 0;
    virtual void register_accepted_iterate(Iterate& iterate) = 0;
 
-   [[nodiscard]] virtual PredictedReductionModel generate_predicted_reduction_model(const Direction& direction) const = 0;
-   [[nodiscard]] size_t get_hessian_evaluation_count() const;
-   [[nodiscard]] size_t get_number_subproblems_solved() const;
-   [[nodiscard]] SecondOrderCorrection soc_strategy() const;
+   [[nodiscard]] virtual size_t get_hessian_evaluation_count() const = 0;
+   [[nodiscard]] virtual size_t get_number_subproblems_solved() const = 0;
+   [[nodiscard]] virtual SecondOrderCorrection soc_strategy() const = 0;
 
 protected:
-   const OptimalityProblem optimality_problem;
-   l1RelaxedProblem relaxed_problem;
-   std::unique_ptr<Subproblem> subproblem;
-
    [[nodiscard]] virtual double compute_infeasibility_measure(Iterate& iterate) = 0;
    static bool is_small_step(const Direction& direction);
    void recover_active_set(const Model& model, Direction& direction);
-
-public:
-   const size_t number_constraints;
 };
 
 #endif //UNO_CONSTRAINTRELAXATIONSTRATEGY_H
