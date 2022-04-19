5454e36c980268452f3a20d7a565a13c5747bff3
Charlie Vanaret <vanaret@zib.de>
2022-04-19 19:47:20 +0200

    Removed ElasticVariables.hpp (moved content to l1RelaxedProblem)

--- uno/ingredients/strategy/OptimalityProblem.hpp
+++ uno/ingredients/strategy/OptimalityProblem.hpp
@@ -4,7 +4,6 @@
 #include <vector>
 #include <cmath>
 #include "NonlinearProblem.hpp"
-#include "ingredients/constraint_relaxation/ElasticVariables.hpp"
 
 class OptimalityProblem: public NonlinearProblem {
 public:
