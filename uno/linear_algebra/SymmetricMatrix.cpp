5c077c1f773ac9e770289d8ffac415be30eae62d
Charlie Vanaret <vanaret@math.tu-berlin.de>
2022-07-22 14:00:52 +0200

    Deleted unused matrix functions

--- uno/linear_algebra/SymmetricMatrix.cpp
+++ uno/linear_algebra/SymmetricMatrix.cpp
@@ -14,21 +14,6 @@ void SymmetricMatrix::reset() {
    this->number_nonzeros = 0;
 }
 
-std::vector<double> SymmetricMatrix::product(const std::vector<double>& vector) const {
-   assert(this->dimension == vector.size() && "The matrix and the vector do not have the same size");
-
-   // create (n, 1) result
-   std::vector<double> result(vector.size());
-   this->for_each([&](size_t i, size_t j, double entry) {
-      result[i] += entry * vector[j];
-      // off-diagonal term
-      if (i != j) {
-         result[j] += entry * vector[i];
-      }
-   });
-   return result;
-}
-
 double SymmetricMatrix::quadratic_product(const std::vector<double>& x, const std::vector<double>& y, size_t block_size) const {
    assert(x.size() == y.size() && "SymmetricMatrix::quadratic_product: the two vectors x and y do not have the same size");
    assert(block_size <= x.size() && "SymmetricMatrix::quadratic_product: the block size is larger than the vectors");
@@ -40,23 +25,6 @@ double SymmetricMatrix::quadratic_product(const std::vector<double>& x, const st
       }
    });
    return result;
-}
-
-void SymmetricMatrix::add_outer_product(const SparseVector<double>& x, double scaling_factor) {
-   // perform matrix addition: A + rho x x^T
-   x.for_each([&](size_t row_index, double row_entry) {
-      x.for_each([&](size_t column_index, double column_entry) {
-         // upper triangular matrix
-         if (row_index <= column_index) {
-            // add product of components
-            this->insert(scaling_factor * row_entry * column_entry, row_index, column_index);
-         }
-      });
-   });
-}
-
-void SymmetricMatrix::finalize(size_t /*column_index*/) {
-   // by default, do nothing
 }
 
 std::ostream& operator<<(std::ostream& stream, const SymmetricMatrix& matrix) {
