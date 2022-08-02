a2e752a342c90317f4e25e06b66c43bba66b397d
Charlie Vanaret <vanaret@math.tu-berlin.de>
2022-08-02 18:09:40 -0500

    Minor refactoring

--- uno/preprocessing/Preprocessing.cpp
+++ uno/preprocessing/Preprocessing.cpp
@@ -7,7 +7,6 @@
 // compute a least-square approximation of the multipliers by solving a linear system (uses existing linear system)
 void Preprocessing::compute_least_square_multipliers(const Model& model, SymmetricMatrix& matrix, std::vector<double>& rhs, LinearSolver& solver,
       Iterate& current_iterate, std::vector<double>& multipliers, double multipliers_max_norm) {
-   const size_t number_variables = current_iterate.primals.size();
    current_iterate.evaluate_objective_gradient(model);
    current_iterate.evaluate_constraint_jacobian(model);
 
@@ -16,16 +15,16 @@ void Preprocessing::compute_least_square_multipliers(const Model& model, Symmetr
    /******************************/
    matrix.reset();
    // identity block
-   for (size_t i = 0; i < number_variables; i++) {
+   for (size_t i = 0; i < model.number_variables; i++) {
       matrix.insert(1., i, i);
       matrix.finalize_column(i);
    }
    // Jacobian of general constraints
    for (size_t j = 0; j < model.number_constraints; j++) {
       current_iterate.original_evaluations.constraint_jacobian[j].for_each([&](size_t i, double derivative) {
-         matrix.insert(derivative, i, number_variables + j);
+         matrix.insert(derivative, i, model.number_variables + j);
       });
-      matrix.finalize_column(number_variables + j);
+      matrix.finalize_column(model.number_variables + j);
    }
    DEBUG << "Matrix for least-square multipliers:\n" << matrix << '\n';
 
@@ -38,7 +37,7 @@ void Preprocessing::compute_least_square_multipliers(const Model& model, Symmetr
       rhs[i] += model.objective_sign * derivative;
    });
    // variable bound constraints
-   for (size_t i = 0; i < number_variables; i++) {
+   for (size_t i = 0; i < model.number_variables; i++) {
       rhs[i] -= current_iterate.multipliers.lower_bounds[i] + current_iterate.multipliers.upper_bounds[i];
    }
    DEBUG << "RHS for least-square multipliers: "; print_vector(DEBUG, rhs, 0, matrix.dimension);
@@ -52,9 +51,9 @@ void Preprocessing::compute_least_square_multipliers(const Model& model, Symmetr
    DEBUG << "Solution: "; print_vector(DEBUG, solution, 0, matrix.dimension); DEBUG << '\n';
 
    // if least-square multipliers too big, discard them. Otherwise, store them
-   if (norm_inf(solution, Range(number_variables, number_variables + model.number_constraints)) <= multipliers_max_norm) {
+   if (norm_inf(solution, Range(model.number_variables, model.number_variables + model.number_constraints)) <= multipliers_max_norm) {
       for (size_t j = 0; j < model.number_constraints; j++) {
-         multipliers[j] = solution[number_variables + j];
+         multipliers[j] = solution[model.number_variables + j];
       }
    }
 }
