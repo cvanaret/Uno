843e4e13c4725680c9fadaf13d668dd9fa7b1521
Charlie Vanaret <vanaret@zib.de>
2022-09-24 13:15:14 +0200

    C wrapper: SparseVector<double> successfully wrapped :)

---
+++ uno/interfaces/C/SparseVectorWrapper.cpp
@@ -0,0 +1,23 @@
+// Copyright (c) 2022 Charlie Vanaret
+// Licensed under the MIT license. See LICENSE file in the project directory for details.
+
+#include <iostream>
+#include "SparseVectorWrapper.hpp"
+
+SparseVector<double>* SparseVector_new(size_t capacity) {
+   return new SparseVector<double>(capacity);
+}
+
+void SparseVector_insert(SparseVector<double>* vector, size_t key, double value) {
+   vector->insert(key, value);
+}
+
+void SparseVector_clear(SparseVector<double>* vector) {
+   vector->clear();
+}
+
+void SparseVector_display(SparseVector<double>* vector) {
+   vector->for_each([](size_t key, double value) {
+      std::cout << "(" << key << ", " << value << ")\n";
+   });
+}
---
+++ uno/interfaces/C/SparseVectorWrapper.hpp
@@ -0,0 +1,23 @@
+// Copyright (c) 2022 Charlie Vanaret
+// Licensed under the MIT license. See LICENSE file in the project directory for details.
+
+#ifndef UNO_SPARSEVECTOR_WRAPPER_H
+#define UNO_SPARSEVECTOR_WRAPPER_H
+
+#include "linear_algebra/SparseVector.hpp"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+   // T = double
+   SparseVector<double>* SparseVector_new(size_t capacity);
+   void SparseVector_insert(SparseVector<double>* vector, size_t key, double value);
+   void SparseVector_clear(SparseVector<double>* vector);
+   void SparseVector_display(SparseVector<double>* vector);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // UNO_SPARSEVECTOR_WRAPPER_H
