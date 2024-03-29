cvanaret/Uno

commit 58bd395af60c04066ed33c2be1ee3bb734a78b6c

QP/LP solver allocated with exact numbers of nonzero elements in objective gradient, Jacobian and Hessian (provided by ASL)

uno/ingredients/constraint_relaxation_strategy/FeasibilityRestoration.cpp
@@ -15,7 +15,8 @@ FeasibilityRestoration::FeasibilityRestoration(const Model& model, const Options
       // create the (restoration phase) feasibility problem (objective multiplier = 0)
       feasibility_problem(model, 0., options.get_double("l1_constraint_violation_coefficient")),
       subproblem(SubproblemFactory::create(this->feasibility_problem.number_variables, this->feasibility_problem.number_constraints,
-            this->feasibility_problem.get_number_jacobian_nonzeros(), this->feasibility_problem.get_number_hessian_nonzeros(), options)),
+            this->feasibility_problem.get_number_objective_gradient_nonzeros(), this->feasibility_problem.get_number_jacobian_nonzeros(),
+            this->feasibility_problem.get_number_hessian_nonzeros(), options)),
       // create the globalization strategies (one for each phase)
       restoration_phase_strategy(GlobalizationStrategyFactory::create(options.get_string("globalization_strategy"), false, options)),
       optimality_phase_strategy(GlobalizationStrategyFactory::create(options.get_string("globalization_strategy"), true, options)),

uno/ingredients/constraint_relaxation_strategy/l1Relaxation.cpp
@@ -20,7 +20,8 @@ l1Relaxation::l1Relaxation(const Model& model, const Options& options) :
       // create the l1 relaxed problem
       l1_relaxed_problem(model, options.get_double("l1_relaxation_initial_parameter"), options.get_double("l1_constraint_violation_coefficient")),
       subproblem(SubproblemFactory::create(this->l1_relaxed_problem.number_variables, this->l1_relaxed_problem.number_constraints,
-            this->l1_relaxed_problem.get_number_jacobian_nonzeros(), this->l1_relaxed_problem.get_number_hessian_nonzeros(), options)),
+            this->l1_relaxed_problem.get_number_objective_gradient_nonzeros(), this->l1_relaxed_problem.get_number_jacobian_nonzeros(),
+            this->l1_relaxed_problem.get_number_hessian_nonzeros(), options)),
       globalization_strategy(GlobalizationStrategyFactory::create(options.get_string("globalization_strategy"), true, options)),
       penalty_parameter(options.get_double("l1_relaxation_initial_parameter")),
       tolerance(options.get_double("tolerance")),

uno/ingredients/subproblem/SubproblemFactory.cpp
@@ -8,20 +8,22 @@
 #include "solvers/QP/QPSolverFactory.hpp"
 #include "solvers/linear/SymmetricIndefiniteLinearSolverFactory.hpp"
 
-std::unique_ptr<Subproblem> SubproblemFactory::create(size_t max_number_variables, size_t max_number_constraints,
-      size_t max_number_jacobian_nonzeros, size_t max_number_hessian_nonzeros, const Options& options) {
+std::unique_ptr<Subproblem> SubproblemFactory::create(size_t number_variables, size_t number_constraints, size_t number_objective_gradient_nonzeros,
+      size_t number_jacobian_nonzeros, size_t number_hessian_nonzeros, const Options& options) {
    const std::string subproblem_strategy = options.get_string("subproblem");
    // active-set methods
    if (subproblem_strategy == "QP") {
-      return std::make_unique<QPSubproblem>(max_number_variables, max_number_constraints, max_number_hessian_nonzeros, options);
+      return std::make_unique<QPSubproblem>(number_variables, number_constraints, number_objective_gradient_nonzeros, number_jacobian_nonzeros,
+            number_hessian_nonzeros, options);
    }
    else if (subproblem_strategy == "LP") {
-      return std::make_unique<LPSubproblem>(max_number_variables, max_number_constraints, options);
+      return std::make_unique<LPSubproblem>(number_variables, number_constraints, number_objective_gradient_nonzeros, number_jacobian_nonzeros,
+            options);
    }
    // interior-point method
    else if (subproblem_strategy == "primal_dual_interior_point") {
-      return std::make_unique<PrimalDualInteriorPointSubproblem>(max_number_variables, max_number_constraints,
-            max_number_jacobian_nonzeros, max_number_hessian_nonzeros, options);
+      return std::make_unique<PrimalDualInteriorPointSubproblem>(number_variables, number_constraints, number_jacobian_nonzeros,
+            number_hessian_nonzeros, options);
    }
    throw std::invalid_argument("Subproblem strategy " + subproblem_strategy + " is not supported");
 }

uno/ingredients/subproblem/SubproblemFactory.hpp
@@ -10,8 +10,8 @@
 
 class SubproblemFactory {
 	public:
-		static std::unique_ptr<Subproblem> create(size_t max_number_variables, size_t max_number_constraints,
-            size_t max_number_jacobian_nonzeros, size_t max_number_hessian_nonzeros, const Options& options);
+		static std::unique_ptr<Subproblem> create(size_t number_variables, size_t number_constraints, size_t number_objective_gradient_nonzeros,
+            size_t number_jacobian_nonzeros, size_t number_hessian_nonzeros, const Options& options);
 
       static std::vector<std::string> available_strategies();
 };

uno/ingredients/subproblem/inequality_constrained_methods/LPSubproblem.cpp
@@ -5,9 +5,11 @@
 #include "LPSubproblem.hpp"
 #include "solvers/LP/LPSolverFactory.hpp"
 
-LPSubproblem::LPSubproblem(size_t max_number_variables, size_t max_number_constraints, const Options& options) :
+LPSubproblem::LPSubproblem(size_t max_number_variables, size_t max_number_constraints, size_t max_number_objective_gradient_nonzeros,
+      size_t max_number_jacobian_nonzeros, const Options& options) :
       InequalityConstrainedMethod(max_number_variables, max_number_constraints),
-      solver(LPSolverFactory::create(max_number_variables, max_number_constraints, options.get_string("LP_solver"), options)) {
+      solver(LPSolverFactory::create(options.get_string("LP_solver"), max_number_variables, max_number_constraints,
+            max_number_objective_gradient_nonzeros, max_number_jacobian_nonzeros, options)) {
 }
 
 void LPSubproblem::generate_initial_iterate(const NonlinearProblem& /*problem*/, Iterate& /*initial_iterate*/) {

uno/ingredients/subproblem/inequality_constrained_methods/LPSubproblem.hpp
@@ -11,7 +11,8 @@
 
 class LPSubproblem : public InequalityConstrainedMethod {
 public:
-   LPSubproblem(size_t max_number_variables, size_t max_number_constraints, const Options& options);
+   LPSubproblem(size_t max_number_variables, size_t max_number_constraints, size_t max_number_objective_gradient_nonzeros,
+         size_t max_number_jacobian_nonzeros, const Options& options);
 
    void generate_initial_iterate(const NonlinearProblem& problem, Iterate& initial_iterate) override;
    [[nodiscard]] Direction solve(Statistics& statistics, const NonlinearProblem& problem, Iterate& current_iterate,

uno/ingredients/subproblem/inequality_constrained_methods/QPSubproblem.cpp
@@ -5,7 +5,8 @@
 #include "preprocessing/Preprocessing.hpp"
 #include "solvers/QP/QPSolverFactory.hpp"
 
-QPSubproblem::QPSubproblem(size_t max_number_variables, size_t max_number_constraints, size_t max_number_hessian_nonzeros, const Options& options) :
+QPSubproblem::QPSubproblem(size_t max_number_variables, size_t max_number_constraints, size_t max_number_objective_gradient_nonzeros,
+      size_t max_number_jacobian_nonzeros, size_t max_number_hessian_nonzeros, const Options& options) :
       InequalityConstrainedMethod(max_number_variables, max_number_constraints),
       use_regularization(options.get_string("globalization_mechanism") != "TR" || options.get_bool("convexify_QP")),
       enforce_linear_constraints_at_initial_iterate(options.get_bool("enforce_linear_constraints")),
@@ -14,6 +15,7 @@ QPSubproblem::QPSubproblem(size_t max_number_variables, size_t max_number_constr
             max_number_hessian_nonzeros + max_number_variables, this->use_regularization, options)),
       // maximum number of Hessian nonzeros = number nonzeros + possible diagonal inertia correction
       solver(QPSolverFactory::create(options.get_string("QP_solver"), max_number_variables, max_number_constraints,
+            max_number_objective_gradient_nonzeros, max_number_jacobian_nonzeros,
             // if the QP solver is used during preprocessing, we need to allocate the Hessian with at least number_variables elements
             std::max(this->enforce_linear_constraints_at_initial_iterate ? max_number_variables : 0, hessian_model->hessian->capacity),
             options)) {

uno/ingredients/subproblem/inequality_constrained_methods/QPSubproblem.hpp
@@ -11,7 +11,8 @@
 
 class QPSubproblem : public InequalityConstrainedMethod {
 public:
-   QPSubproblem(size_t max_number_variables, size_t max_number_constraints, size_t max_number_hessian_nonzeros, const Options& options);
+   QPSubproblem(size_t max_number_variables, size_t max_number_constraints, size_t max_number_objective_gradient_nonzeros,
+         size_t max_number_jacobian_nonzeros, size_t max_number_hessian_nonzeros, const Options& options);
 
    void initialize_statistics(Statistics& statistics, const Options& options) override;
    void generate_initial_iterate(const NonlinearProblem& problem, Iterate& initial_iterate) override;
@@ -23,7 +24,7 @@ public:
 
 protected:
    const bool use_regularization;
-   bool enforce_linear_constraints_at_initial_iterate;
+   const bool enforce_linear_constraints_at_initial_iterate;
    // pointers to allow polymorphism
    const std::unique_ptr<HessianModel> hessian_model; /*!< Strategy to evaluate or approximate the Hessian */
    const std::unique_ptr<QPSolver> solver; /*!< Solver that solves the subproblem */

commit 5bcf0236ec2a9b8d1f66b29dfb6f7cd265241353

Fixed bug introduced in 58bd395af60c04066ed33c2be1ee3bb734a78b6c: subproblem solver allocated with the correct dimenions (numbers of nonzeros)

uno/ingredients/constraint_relaxation_strategy/FeasibilityRestoration.cpp
@@ -14,9 +14,13 @@ FeasibilityRestoration::FeasibilityRestoration(const Model& model, const Options
       optimality_problem(model),
       // create the (restoration phase) feasibility problem (objective multiplier = 0)
       feasibility_problem(model, 0., options.get_double("l1_constraint_violation_coefficient")),
-      subproblem(SubproblemFactory::create(this->feasibility_problem.number_variables, this->feasibility_problem.number_constraints,
-            this->feasibility_problem.number_objective_gradient_nonzeros(), this->feasibility_problem.number_jacobian_nonzeros(),
-            this->feasibility_problem.number_hessian_nonzeros(), options)),
+      subproblem(SubproblemFactory::create(
+            std::max(this->optimality_problem.number_variables, this->feasibility_problem.number_variables),
+            std::max(this->optimality_problem.number_constraints, this->feasibility_problem.number_constraints),
+            std::max(this->optimality_problem.number_objective_gradient_nonzeros(), this->feasibility_problem.number_objective_gradient_nonzeros()),
+            std::max(this->optimality_problem.number_jacobian_nonzeros(), this->feasibility_problem.number_jacobian_nonzeros()),
+            std::max(this->optimality_problem.number_hessian_nonzeros(), this->feasibility_problem.number_hessian_nonzeros()),
+            options)),
       // create the globalization strategies (one for each phase)
       restoration_phase_strategy(GlobalizationStrategyFactory::create(options.get_string("globalization_strategy"), false, options)),
       optimality_phase_strategy(GlobalizationStrategyFactory::create(options.get_string("globalization_strategy"), true, options)),