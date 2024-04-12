f629f2fa2bde9daae5eadc7e372d990e5865c6d2
Charlie Vanaret <vanaret@zib.de>
2024-04-12 17:48:25 +0200

    Added unit test for COOSymmetricMatrix

--- unotest/COOSymmetricMatrixTests.cpp
+++ unotest/COOSymmetricMatrixTests.cpp
@@ -3,3 +3,23 @@
 
 #include <gtest/gtest.h>
 #include "linear_algebra/COOSymmetricMatrix.hpp"
+
+const size_t n = 5;
+const size_t nnz = 7;
+
+COOSymmetricMatrix<double> create_COO_matrix() {
+   COOSymmetricMatrix<double> matrix(n, nnz, false);
+   matrix.insert(2., 0, 0);
+   matrix.insert(3., 0, 1);
+   matrix.insert(4., 1, 2);
+   matrix.insert(6., 1, 4);
+   matrix.insert(1., 2, 2);
+   matrix.insert(5., 2, 3);
+   matrix.insert(1., 4, 4);
+   return matrix;
+}
+
+TEST(COOSymmetricMatrix, NNZ) {
+   const COOSymmetricMatrix<double> matrix = create_COO_matrix();
+   ASSERT_EQ(matrix.number_nonzeros, nnz);
+}
