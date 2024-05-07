107f250caa2b5fd292c6730487ac6521980fa112
Charlie Vanaret <vanaret@zib.de>
2024-05-07 20:55:56 +0200

    Filter visualization: acceptable area shown in green

--- visualization/filter.py
+++ visualization/filter.py
@@ -20,44 +20,43 @@ margin_infeasibility = np.append(margin_infeasibility, infeasibility_upper_bound
 inner_cusps = unconstrained_merit - envelope_slope*margin_infeasibility[1:]
 
 # colors
-filter_borders_color = '#666666' # gray
-filter_envelope_color = '#cccccc' # lightgray
+filter_borders_color = 'black' # gray
+# filter_envelope_color = '#cccccc' # lightgray
+entries_color = 'black'
+upper_bound_color = 'r'
+acceptable_area_color = 'g'
 
 # filter entries
-plt.scatter(infeasibility, unconstrained_merit)
+plt.scatter(infeasibility, unconstrained_merit, c=entries_color)
 # infeasibility upper bound
-plt.axvline(x=infeasibility_upper_bound, color='r', linestyle='-')
+plt.axvline(x=infeasibility_upper_bound, color=upper_bound_color, linestyle='-')
 plt.gca().set_xlim(left=0.)
 
 def merge_lists(lst1, lst2):
    return np.array([[i, j] for i, j in zip(lst1, lst2)]).ravel()
 
-# filter borders and envelope
+# filter borders
 number_entries = len(infeasibility)
 for i in range(number_entries):
-   # filter borders
    if 0 < i:
-      plt.vlines(infeasibility[i], unconstrained_merit[i], unconstrained_merit[i-1], colors=filter_borders_color)
+      plt.vlines(x=infeasibility[i], ymin=unconstrained_merit[i], ymax=unconstrained_merit[i-1], colors=filter_borders_color)
    if i < number_entries-1:
       plt.hlines(unconstrained_merit[i], infeasibility[i], infeasibility[i+1], colors=filter_borders_color)
-   
-   # envelope (vertical)
-   #if 0 < i:
-   #   plt.vlines(infeasibility_margin*infeasibility[i], outer_cusps[i], inner_cusps[i-1], colors=filter_envelope_color)
-   # envelope (slope): y <= unconstrained_merit[i] - envelope_slope * x
-   #plt.plot([margin_infeasibility[i], margin_infeasibility[i+1]], [outer_cusps[i], inner_cusps[i]], color=filter_envelope_color)
-   
 plt.hlines(unconstrained_merit[-1], infeasibility[-1], infeasibility_upper_bound, colors=filter_borders_color)
+
+(ymin, ymax) = plt.ylim()
+
+# first vertical line
+plt.vlines(x=infeasibility[0], ymin=unconstrained_merit[0], ymax=ymax, colors=filter_borders_color)
 
 # acceptable area
 x = merge_lists(margin_infeasibility, margin_infeasibility)
 x = np.insert(x, 0, 0.)
 x = x[0:-1]
 y1 = merge_lists(outer_cusps, inner_cusps)
-(min_y, max_y) = plt.ylim()
-y1 = np.insert(y1, 0, max_y)
-y1 = np.insert(y1, 0, max_y)
-plt.fill_between(x, y1=y1, y2=min_y, facecolor='g', alpha=0.3)
+y1 = np.insert(y1, 0, ymax)
+y1 = np.insert(y1, 0, ymax)
+plt.fill_between(x, y1=y1, y2=ymin, facecolor=acceptable_area_color, alpha=0.3)
 
 plt.xlabel("infeasibility")
 plt.ylabel("unconstrained merit")
