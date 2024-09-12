56cd4bb23f1822eb7773175b958145c52a2b2ea3
Dr. Charlie Vanaret <vanaret@zib.de>
2024-09-12 20:18:01 +0200

    FunnelMethod: renamed first argument of is_infeasibility_sufficiently_reduced

--- uno/ingredients/globalization_strategy/FunnelMethod.hpp
+++ uno/ingredients/globalization_strategy/FunnelMethod.hpp
@@ -35,7 +35,7 @@ namespace uno {
    
       [[nodiscard]] bool is_iterate_acceptable(Statistics& statistics, const ProgressMeasures& current_progress,
             const ProgressMeasures& trial_progress, const ProgressMeasures& predicted_reduction, double objective_multiplier) override;
-      [[nodiscard]] bool is_infeasibility_sufficiently_reduced(const ProgressMeasures& current_progress, const ProgressMeasures& trial_progress) const override;
+      [[nodiscard]] bool is_infeasibility_sufficiently_reduced(const ProgressMeasures& reference_progress, const ProgressMeasures& trial_progress) const override;
       
       void initialize(Statistics& statistics, const Iterate& initial_iterate, const Options& options) override;
       void reset() override;
