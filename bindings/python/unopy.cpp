020fec2c171080bb68c0fad092b800a7094d4a84
Charlie Vanaret <vanaret@zib.de>
2024-09-19 10:54:58 +0200

    unopy: finished binding to SparseVector

--- bindings/python/unopy.cpp
+++ bindings/python/unopy.cpp
@@ -9,11 +9,14 @@ namespace uno {
       
       // SparseVector class (instantiated with double elements)
       py::class_<SparseVector<double>>(module, "SparseVector")
-         .def(py::init<size_t>())
-         .def("size", &SparseVector<double>::size, "")
-         .def("insert", &SparseVector<double>::insert, "")
-         .def("clear", &SparseVector<double>::clear, "")
-         .def("is_empty", &SparseVector<double>::is_empty, "")
+         // constructor
+         .def(py::init<size_t>(), py::arg("capacity"))
+         // methods
+         .def("size", &SparseVector<double>::size, "Number of elements of the sparse vector")
+         .def("insert", &SparseVector<double>::insert, "Insert an element into the sparse vector")
+         .def("clear", &SparseVector<double>::clear, "Delete all elements in the sparse vector")
+         .def("is_empty", &SparseVector<double>::is_empty, "True if the sparse vector is empty, false otherwise")
+         // string representation
          .def("__repr__", [](const SparseVector<double>& vector) {
             std::string representation = "Sparse vector:\n";
             for (const auto [index, value]: vector) {
