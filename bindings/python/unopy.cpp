000384e1450013650fcf9f892e80249f3ee07490
Charlie Vanaret <vanaret@zib.de>
2024-09-19 15:47:49 +0200

    Added pybind definition to RectangularMatrix

--- bindings/python/unopy.cpp
+++ bindings/python/unopy.cpp
@@ -5,6 +5,7 @@ namespace py = pybind11;
 namespace uno {
    // individual classes
    void define_SparseVector(py::module& module);
+   void define_RectangularMatrix(py::module& module);
    void define_Options(py::module& module);
 
    // module definition
@@ -12,6 +13,7 @@ namespace uno {
       module.doc() = "Python binding to the solver Uno for nonconvex optimization";
       
       define_SparseVector(module);
+      define_RectangularMatrix(module);
       define_Options(module);
    }
 } // namespace
