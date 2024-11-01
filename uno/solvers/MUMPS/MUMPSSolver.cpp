2d156a47f970e954265696b31e640948b47973ac
Charlie Vanaret <vanaret@zib.de>
2024-11-01 11:47:05 +0100

    Set correct options in MUMPS to better detect null pivots (singular matrices)

--- uno/solvers/MUMPS/MUMPSSolver.cpp
+++ uno/solvers/MUMPS/MUMPSSolver.cpp
@@ -28,6 +28,8 @@ namespace uno {
       this->mumps_structure.icntl[1] = -1;
       this->mumps_structure.icntl[2] = -1;
       this->mumps_structure.icntl[3] = 0;
+      this->mumps_structure.icntl[12] = 1;
+      this->mumps_structure.icntl[23] = 1; // ICNTL(24) controls the detection of “null pivot rows”
    }
 
    MUMPSSolver::~MUMPSSolver() {
