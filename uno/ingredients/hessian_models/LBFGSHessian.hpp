7bca621633330937736a5143d3bf82c9ad38ecaf
Charlie Vanaret <vanaret@zib.de>
2025-04-03 23:19:15 +0200

    Created empty class for L-BFGS Hessian model (conditional on LAPACK) + functional test for LAPACK's Cholesky factorization routine

---
+++ uno/ingredients/hessian_models/LBFGSHessian.hpp
@@ -0,0 +1,23 @@
+// Copyright (c) 2025 Charlie Vanaret
+// Licensed under the MIT license. See LICENSE file in the project directory for details.
+
+#ifndef UNO_LBFGSHESSIAN_H
+#define UNO_LBFGSHESSIAN_H
+
+#include "HessianModel.hpp"
+
+namespace uno {
+   class LBFGSHessian : public HessianModel {
+   public:
+      LBFGSHessian();
+      ~LBFGSHessian() override = default;
+
+      void initialize_statistics(Statistics& statistics, const Options& options) const override;
+      void evaluate_hessian(Statistics& statistics, const OptimizationProblem& problem, const Vector<double>& primal_variables,
+            const Vector<double>& constraint_multipliers, SymmetricMatrix<size_t, double>& hessian) override;
+      void compute_hessian_vector_product(const OptimizationProblem& problem, const Vector<double>& vector,
+         const Vector<double>& constraint_multipliers, Vector<double>& result) override;
+   };
+} // namespace
+
+#endif // UNO_LBFGSHESSIAN_H
