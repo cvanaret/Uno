68a73d6366ad14b1429928c026d9d0d299efaaaa
Charlie Vanaret <vanaret@zib.de>
2025-04-04 11:21:37 +0200

    Added function notify_accepted_iterate() in HessianModel and subclasses. Will be used by LBFGSHessian

--- uno/ingredients/hessian_models/IdentityHessian.hpp
+++ uno/ingredients/hessian_models/IdentityHessian.hpp
@@ -18,6 +18,8 @@ namespace uno {
       void compute_sparsity(uno_int* row_indices, uno_int* column_indices, uno_int solver_indexing) const override;
       [[nodiscard]] bool is_positive_definite() const override;
 
+      void initialize(const Model& model) override;
+      void initialize_statistics(Statistics& statistics, const Options& options) const override;
       void evaluate_hessian(Statistics& statistics, const Vector<double>& primal_variables,
          double objective_multiplier, const Vector<double>& constraint_multipliers, double* hessian_values) override;
       void compute_hessian_vector_product(const double* x, const double* vector, double objective_multiplier,
