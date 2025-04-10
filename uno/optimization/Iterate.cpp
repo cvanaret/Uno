ceb16c7f3f9860c0077195ed8bae6d7e57f8fd23
Charlie Vanaret <vanaret@zib.de>
2025-04-10 21:27:56 +0200

    Introduced class AssembledLagrangianGradient (wrapper around LagrangianGradient and objective multiplier)

--- uno/optimization/Iterate.cpp
+++ uno/optimization/Iterate.cpp
@@ -91,10 +91,10 @@ namespace uno {
 
       stream << "          ┌ Stationarity: " << iterate.residuals.stationarity << '\n';
       stream << "Residuals │ Complementarity: " << iterate.residuals.complementarity << '\n';
-      //stream << "          └ Lagrangian gradient: " << iterate.residuals.lagrangian_gradient;
+      stream << "          └ Lagrangian gradient: " << iterate.residuals.lagrangian_gradient.assemble(1.);
       stream << "Feasibility residuals ┌ Stationarity: " << iterate.feasibility_residuals.stationarity << '\n';
       stream << "                      │ Complementarity: " << iterate.feasibility_residuals.complementarity << '\n';
-      //stream << "                      └ Lagrangian gradient: " << iterate.feasibility_residuals.lagrangian_gradient;
+      stream << "                      └ Lagrangian gradient: " << iterate.feasibility_residuals.lagrangian_gradient.assemble(1.);
 
       stream << "                  ┌ Infeasibility: " << iterate.progress.infeasibility << '\n';
       stream << "Progress measures │ Optimality: " << iterate.progress.objective(1.) << '\n';
--- uno/optimization/LagrangianGradient.hpp
+++ uno/optimization/LagrangianGradient.hpp
@@ -8,6 +8,55 @@
 #include "linear_algebra/Vector.hpp"
 
 namespace uno {
+   // forward declaration
+   template <typename ElementType>
+   class LagrangianGradient;
+
+   // The LagrangianGradient class represents the dense Lagrangian gradient broken into:
+   // - the objective contribution \nabla f(x_k)
+   // - the constraint contribution -\nabla c(x_k) y_k - z_k
+   // The two contributions can be assembled with a given objective multiplier \rho (see assemble(double)):
+   // \rho \nabla f(x_k) - \nabla c(x_k) y_k - z_k
+   // The resulting object, an AssembledLagrangianGradient, is a wrapper around the LagrangianGradient and rho
+
+   template <typename ElementType>
+   class AssembledLagrangianGradient {
+   public:
+      AssembledLagrangianGradient(const LagrangianGradient<ElementType>& lagrangian_gradient, ElementType objective_multiplier);
+
+      [[nodiscard]] size_t size() const;
+      [[nodiscard]] ElementType operator[](size_t variable_index) const;
+
+   protected:
+      const LagrangianGradient<ElementType>& lagrangian_gradient;
+      const ElementType objective_multiplier;
+   };
+
+   template<typename ElementType>
+   AssembledLagrangianGradient<ElementType>::AssembledLagrangianGradient(const LagrangianGradient<ElementType> &lagrangian_gradient,
+      ElementType objective_multiplier): lagrangian_gradient(lagrangian_gradient), objective_multiplier(objective_multiplier) { }
+
+   template <typename ElementType>
+   size_t AssembledLagrangianGradient<ElementType>::size() const {
+      return this->lagrangian_gradient.size();
+   }
+
+  // access i-th element
+  template <typename ElementType>
+  ElementType AssembledLagrangianGradient<ElementType>::operator[](size_t variable_index) const {
+     return this->objective_multiplier * this->lagrangian_gradient.objective_contribution[variable_index] +
+        this->lagrangian_gradient.constraints_contribution[variable_index];
+  }
+
+   template <typename ElementType>
+   std::ostream& operator<<(std::ostream& stream, const AssembledLagrangianGradient<ElementType>& gradient) {
+      for (size_t variable_index: Range(gradient.size())) {
+         stream << gradient[variable_index] << ' ';
+      }
+      stream << '\n';
+      return stream;
+   }
+
    // Gradient of the Lagrangian
    // Keep the objective and constraint contributions separate. This helps:
    // - computing the KKT and FJ stationarity conditions
@@ -22,8 +71,8 @@ namespace uno {
 
       explicit LagrangianGradient(size_t number_variables);
       [[nodiscard]] size_t size() const;
-      //[[nodiscard]] ElementType operator[](size_t variable_index) const;
       void resize(size_t number_variables);
+      AssembledLagrangianGradient<ElementType> assemble(double objective_multiplier) const;
    };
 
    template <typename ElementType>
@@ -37,30 +86,16 @@ namespace uno {
       return this->objective_contribution.size();
    }
 
-   /*
-   // access i-th element
-   template <typename ElementType>
-   ElementType LagrangianGradient<ElementType>::operator[](size_t variable_index) const {
-      return this->objective_contribution[variable_index] + this->constraints_contribution[variable_index];
-   }
-   */
-
    template <typename ElementType>
    void LagrangianGradient<ElementType>::resize(size_t number_variables) {
       this->objective_contribution.resize(number_variables);
       this->constraints_contribution.resize(number_variables);
    }
 
-   /*
    template <typename ElementType>
-   std::ostream& operator<<(std::ostream& stream, const LagrangianGradient<ElementType>& gradient) {
-      for (size_t variable_index: Range(gradient.constraints_contribution.size())) {
-         stream << gradient[variable_index] << ' ';
-      }
-      stream << '\n';
-      return stream;
+   AssembledLagrangianGradient<ElementType> LagrangianGradient<ElementType>::assemble(double objective_multiplier) const {
+      return {*this, objective_multiplier};
    }
-   */
 } // namespace
 
 #endif // UNO_LAGRANGIANGRADIENT_H
