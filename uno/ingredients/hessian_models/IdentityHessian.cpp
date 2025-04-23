b36b383cf6ed0743119c7539365c313843b63b31
Charlie Vanaret <vanaret@zib.de>
2025-04-24 00:25:10 +0200

    Some progress on L-BFGS

--- uno/ingredients/hessian_models/IdentityHessian.cpp
+++ uno/ingredients/hessian_models/IdentityHessian.cpp
@@ -40,6 +40,9 @@ namespace uno {
    void IdentityHessian::initialize_statistics(Statistics& /*statistics*/, const Options& /*options*/) const {
    }
 
+   void IdentityHessian::notify_accepted_iterate(const Iterate& /*current_iterate*/, const Iterate& /*trial_iterate*/) {
+   }
+
    void IdentityHessian::evaluate_hessian(Statistics& /*statistics*/, const Model& model, const Vector<double>& /*primal_variables*/,
          double /*objective_multiplier*/, const Vector<double>& /*constraint_multipliers*/, double* hessian_values) {
       DEBUG << "Setting identity Hessian\n";
