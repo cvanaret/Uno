54ca74ffe5d9a9e2f342ae0ba83238518bbd3e96
Charlie Vanaret <vanaret@zib.de>
2025-04-26 23:30:17 +0200

    Explicitlt link LAPACK to GTest target

--- unotest/functional_tests/LAPACKTests.cpp
+++ unotest/functional_tests/LAPACKTests.cpp
@@ -1,7 +1,6 @@
 // Copyright (c) 2025 Charlie Vanaret
 // Licensed under the MIT license. See LICENSE file in the project directory for details.
 
-#include <math.h>
 #include <valarray>
 #include <gtest/gtest.h>
 #include "linear_algebra/DenseMatrix.hpp"
