fafed7ddf190a22a9d5e369617450d885d4f935f
Charlie Vanaret <vanaret@zib.de>
2025-05-07 14:09:36 +0200

    Forgot test in ModelFactory

--- uno/model/ModelFactory.cpp
+++ uno/model/ModelFactory.cpp
@@ -9,18 +9,16 @@
 
 namespace uno {
    // note: ownership of the pointer is transferred
-   std::unique_ptr<Model> ModelFactory::reformulate(std::unique_ptr<Model> model, size_t number_bound_constraints, const Options& options) {
-      if (0 < number_bound_constraints) {
-         if (options.get_string("inequality_handling_method") == "primal_dual_interior_point") {
-            // move the fixed variables to the set of general constraints
-            if (!model->get_fixed_variables().empty()) {
-               model = std::make_unique<FixedBoundsConstraintsModel>(std::move(model), options);
-            }
-            // if an equality-constrained problem is required (e.g. interior points or AL), reformulate the model with slacks
-            model = std::make_unique<HomogeneousEqualityConstrainedModel>(std::move(model));
-            // slightly relax the bound constraints
-            model = std::make_unique<BoundRelaxedModel>(std::move(model), options);
+   std::unique_ptr<Model> ModelFactory::reformulate(std::unique_ptr<Model> model, size_t /*number_bound_constraints*/, const Options& options) {
+      if (options.get_string("inequality_handling_method") == "primal_dual_interior_point") {
+         // move the fixed variables to the set of general constraints
+         if (!model->get_fixed_variables().empty()) {
+            model = std::make_unique<FixedBoundsConstraintsModel>(std::move(model), options);
          }
+         // if an equality-constrained problem is required (e.g. interior points or AL), reformulate the model with slacks
+         model = std::make_unique<HomogeneousEqualityConstrainedModel>(std::move(model));
+         // slightly relax the bound constraints
+         model = std::make_unique<BoundRelaxedModel>(std::move(model), options);
       }
       return model;
    }
