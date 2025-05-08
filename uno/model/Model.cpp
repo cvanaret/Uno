fb3fb0d746ff4ca7c14b1cdba31f2526dfb32298
Charlie Vanaret <vanaret@zib.de>
2025-05-08 13:11:51 +0200

    When fixed objective multiplier in LBFGSHessian, compute Lag gradient with the obj multiplier

--- uno/model/Model.cpp
+++ uno/model/Model.cpp
@@ -32,30 +32,51 @@ namespace uno {
       return std::max(lower_bound_violation, upper_bound_violation);
    }
 
+   void Model::evaluate_lagrangian_gradient(Vector<double>& lagrangian_gradient, const Iterate& iterate,
+         const Multipliers& multipliers, double objective_multiplier) const {
+      lagrangian_gradient.fill(0.);
+      // compute and scale the objective contribution
+      if (objective_multiplier != 0.) {
+         this->evaluate_lagrangian_gradient_objective(lagrangian_gradient, iterate);
+         lagrangian_gradient.scale(objective_multiplier);
+      }
+      // add the constraints contribution
+      this->evaluate_lagrangian_gradient_constraints(lagrangian_gradient, iterate, multipliers);
+   }
+
    // Lagrangian gradient split in two parts: objective contribution and constraints' contribution
    void Model::evaluate_lagrangian_gradient(LagrangianGradient<double>& lagrangian_gradient, const Iterate& iterate,
          const Multipliers& multipliers) const {
       lagrangian_gradient.objective_contribution.fill(0.);
       lagrangian_gradient.constraints_contribution.fill(0.);
+      this->evaluate_lagrangian_gradient_objective(lagrangian_gradient.objective_contribution, iterate);
+      this->evaluate_lagrangian_gradient_constraints(lagrangian_gradient.constraints_contribution, iterate, multipliers);
+   }
 
+   // protected member functions
+
+   // Lagrangian gradient split in two parts: objective contribution and constraints' contribution
+   void Model::evaluate_lagrangian_gradient_objective(Vector<double>& objective_contribution, const Iterate& iterate) const {
       // objective gradient
       for (auto [variable_index, derivative]: iterate.evaluations.objective_gradient) {
-         lagrangian_gradient.objective_contribution[variable_index] += derivative;
+         objective_contribution[variable_index] += derivative;
       }
+   }
 
+   // Lagrangian gradient split in two parts: objective contribution and constraints' contribution
+   void Model::evaluate_lagrangian_gradient_constraints(Vector<double>& constraints_contribution, const Iterate& iterate,
+         const Multipliers& multipliers) const {
       // constraints
       for (size_t constraint_index: Range(this->number_constraints)) {
          if (multipliers.constraints[constraint_index] != 0.) {
             for (auto [variable_index, derivative]: iterate.evaluations.constraint_jacobian[constraint_index]) {
-               lagrangian_gradient.constraints_contribution[variable_index] -= multipliers.constraints[constraint_index] * derivative;
+               constraints_contribution[variable_index] -= multipliers.constraints[constraint_index] * derivative;
             }
          }
       }
-
-      // bound constraints of original variables
+      // bound constraints
       for (size_t variable_index: Range(this->number_variables)) {
-         lagrangian_gradient.constraints_contribution[variable_index] -= (multipliers.lower_bounds[variable_index] +
-            multipliers.upper_bounds[variable_index]);
+         constraints_contribution[variable_index] -= (multipliers.lower_bounds[variable_index] + multipliers.upper_bounds[variable_index]);
       }
    }
 } // namespace
