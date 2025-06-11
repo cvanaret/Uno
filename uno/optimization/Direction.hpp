92b846bd26c30daaa4211e14d40a315bbbc1924e
Charlie Vanaret <vanaret@zib.de>
2025-06-11 16:06:24 +0200

    First draft for LPEQP method based on our 2023 work. TODO: write EQP problem

--- uno/optimization/Direction.hpp
+++ uno/optimization/Direction.hpp
@@ -10,6 +10,25 @@
 #include "tools/Infinity.hpp"
 
 namespace uno {
+   struct ActiveConstraints {
+      std::vector<size_t> at_lower_bound{}; /*!< List of constraint indices at their lower bound */
+      std::vector<size_t> at_upper_bound{}; /*!< List of constraint indices at their upper bound */
+
+      explicit ActiveConstraints(size_t capacity) {
+         this->at_lower_bound.reserve(capacity);
+         this->at_upper_bound.reserve(capacity);
+      }
+      ActiveConstraints() = default;
+   };
+
+   struct ActiveSet {
+      ActiveConstraints constraints{}; /*!< List of general constraints */
+      ActiveConstraints bounds{}; /*!< List of bound constraints */
+
+      ActiveSet(size_t number_variables, size_t number_constraints): constraints(number_constraints), bounds(number_variables) { }
+      ActiveSet() = default;
+   };
+
    class Direction {
    public:
       Direction(size_t number_variables, size_t number_constraints);
@@ -25,6 +44,7 @@ namespace uno {
 
       double norm{INF<double>}; /*!< Norm of \f$x\f$ */
       double subproblem_objective{INF<double>}; /*!< Objective value */
+      ActiveSet active_set{};
 
       void set_dimensions(size_t new_number_variables, size_t new_number_constraints);
       void reset();
