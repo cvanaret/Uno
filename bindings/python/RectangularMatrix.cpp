490f876a964558dccc3fec20b28f5878fe94d307
Charlie Vanaret <vanaret@zib.de>
2025-07-01 23:23:24 +0200

    Updated Options.cpp and RectangularMatrix.cpp

--- bindings/python/RectangularMatrix.cpp
+++ bindings/python/RectangularMatrix.cpp
@@ -10,8 +10,8 @@ namespace uno {
          // constructor
          .def(py::init<size_t, size_t>(), py::arg("number_rows"), py::arg("number_columns"), "Constructor")
          // methods
-         .def("get_number_rows", &RectangularMatrix<double>::get_number_rows, "Number of rows")
-         .def("get_number_columns", &RectangularMatrix<double>::get_number_columns, "Number of columns")
+         //.def("get_number_rows", &RectangularMatrix<double>::get_number_rows, "Number of rows")
+         //.def("get_number_columns", &RectangularMatrix<double>::get_number_columns, "Number of columns")
          .def("clear", &RectangularMatrix<double>::clear, "Empty the matrix")
          .def("__getitem__", [](RectangularMatrix<double>& matrix, size_t row_index) -> SparseVector<double>& {
             return matrix[row_index];
