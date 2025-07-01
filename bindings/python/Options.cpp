c5bf500f0989f9002fb7157eef973b3ec87e3bd1
Charlie Vanaret <vanaret@zib.de>
2025-07-01 23:23:24 +0200

    Updated Options.cpp and RectangularMatrix.cpp

--- bindings/python/Options.cpp
+++ bindings/python/Options.cpp
@@ -1,5 +1,5 @@
 #include <pybind11/pybind11.h>
-#include "tools/Options.hpp"
+#include "options/Options.hpp"
 
 namespace py = pybind11;
 
@@ -8,17 +8,19 @@ namespace uno {
       // Options class
       py::class_<Options>(module, "Options")
          // constructor
-         .def(py::init<>(), "Constructor")
+         .def(py::init<bool>(), py::arg("are_default_options"), "Constructor")
          // methods
          .def("__setitem__", [](Options& options, const std::string& key, const std::string& value) {
             options[key] = value;
          }, py::arg("key"), py::arg("value"), "Set an option")
-         .def("__getitem__", &Options::at, py::arg("key"), "Read an option")
-         .def_static("get_default_options", &Options::get_default_options, py::arg("file_path"), "Read default options from an option file")
+         //.def("__getitem__", &Options::get_string, py::arg("key"), "Read an option")
+         //.def_static("get_default_options", &Options::get_default_options, py::arg("file_path"), "Read default options from an option file")
          // string representation
+         /*
          .def("__repr__", [](const Options& options) {
             return options.to_string(false);
          })
+         */
       ;
    }
 } // namespace
