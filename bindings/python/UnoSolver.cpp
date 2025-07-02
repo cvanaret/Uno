fe5610b54e8ac84764b6e7a33cfda28742b63ac6
Charlie Vanaret <vanaret@zib.de>
2025-07-02 21:07:13 +0200

    Started a PythonModel that should be created on the Python side

---
+++ bindings/python/UnoSolver.cpp
@@ -0,0 +1,21 @@
+#include <pybind11/pybind11.h>
+#include "Uno.hpp"
+#include "model/Model.hpp"
+#include "optimization/Iterate.hpp"
+#include "options/Options.hpp"
+
+namespace py = pybind11;
+
+namespace uno {
+   void define_UnoSolver(py::module& module) {
+      // Uno class
+      py::class_<Uno>(module, "UnoSolver")
+         // constructor
+         .def(py::init<size_t, const Options&>(), py::arg("number_constraints"), py::arg("options"), "Constructor")
+         // methods
+         .def("solve", py::overload_cast<const Model&, Iterate&, const Options&>(&Uno::solve),
+            py::arg("model"), py::arg("iterate"), py::arg("options"),
+            "Solve an optimization model with the Uno solver");
+      ;
+   }
+} // namespace
