885b18818e926e83e2bc68e4a0cbb60afc2fac63
Charlie Vanaret <vanaret@zib.de>
2025-07-03 07:48:36 +0200

    Fixed missing include

--- uno/optimization/Multipliers.cpp
+++ uno/optimization/Multipliers.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) 2018-2024 Charlie Vanaret
 // Licensed under the MIT license. See LICENSE file in the project directory for details.
 
+#include <cmath>
 #include "Multipliers.hpp"
 
 namespace uno {
