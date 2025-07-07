2e65770934ad7ebf91e1643d079e560b6a2b98e2
Charlie Vanaret <vanaret@zib.de>
2025-07-07 12:11:36 +0200

    Added directories for classes and modules + added SymmetricMatrix module (to be tested)

--- bindings/python/unopy.cpp
+++ bindings/python/unopy.cpp
@@ -1,3 +1,6 @@
+// Copyright (c) 2025 Charlie Vanaret
+// Licensed under the MIT license. See LICENSE file in the project directory for details.
+
 #include <pybind11/pybind11.h>
 
 namespace py = pybind11;
@@ -6,16 +9,18 @@ namespace uno {
    // individual classes
    void define_SparseVector(py::module& module);
    void define_RectangularMatrix(py::module& module);
+   void define_SymmetricMatrix(py::module& module);
    void define_Options(py::module& module);
    //void define_Model(py::module& module);
    //void define_UnoSolver(py::module& module);
 
    // module definition
    PYBIND11_MODULE(unopy, module) {
-      module.doc() = "Python binding to the solver Uno for nonlinearly constrained optimization";
+      module.doc() = "Python bindings to the solver Uno for nonlinearly constrained optimization";
       
       define_SparseVector(module);
       define_RectangularMatrix(module);
+      define_SymmetricMatrix(module);
       define_Options(module);
       //define_Model(module);
       //define_UnoSolver(module);
