965126d1dcaad8df330e2839f0266f26625ffd01
Charlie Vanaret <vanaret@zib.de>
2025-07-08 10:49:12 +0200

    Moved bindings/python/CMakeLists.txt into main CMakeLists.txt

--- bindings/python/modules/SymmetricMatrix.cpp
+++ bindings/python/modules/SymmetricMatrix.cpp
@@ -8,18 +8,13 @@ namespace py = pybind11;
 
 namespace uno {
    void define_SymmetricMatrix(py::module& module) {
-      // RectangularMatrix class (instantiated with double elements)
+      // SymmetricMatrix class (instantiated with size_t indices and double elements)
       py::class_<SymmetricMatrix<size_t, double>>(module, "SymmetricMatrix")
          // abstract class, no constructor
          // methods
-         //.def("get_number_rows", &RectangularMatrix<double>::get_number_rows, "Number of rows")
-         //.def("get_number_columns", &RectangularMatrix<double>::get_number_columns, "Number of columns")
          .def("insert", &SymmetricMatrix<size_t, double>::insert, py::arg("element"), py::arg("row_index"),
-            py::arg("column_index"), "Insert an element");
-         /*
-         .def("__getitem__", [](RectangularMatrix<double>& matrix, size_t row_index) -> SparseVector<double>& {
-            return matrix[row_index];
-         }, py::arg("row_index"), "Returns a row", py::return_value_policy::reference_internal);
-         */
+            py::arg("column_index"), "Insert an element")
+         .def("finalize_column", &SymmetricMatrix<size_t, double>::finalize_column, py::arg("column_index"),
+            "Finalize a given column");
    }
 } // namespace
