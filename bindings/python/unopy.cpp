95f128145765dfca9505b427d9bf4bb8ed6a884d
Charlie Vanaret <vanaret@zib.de>
2025-07-11 12:13:38 +0200

    Minor changes

--- bindings/python/unopy.cpp
+++ bindings/python/unopy.cpp
@@ -12,7 +12,7 @@ namespace uno {
    void define_SymmetricMatrix(py::module& module);
    void define_Options(py::module& module);
    //void define_Model(py::module& module);
-   //void define_UnoSolver(py::module& module);
+   void define_UnoSolver(py::module& module);
 
    // module definition
    PYBIND11_MODULE(unopy, module) {
@@ -23,6 +23,6 @@ namespace uno {
       define_SymmetricMatrix(module);
       define_Options(module);
       //define_Model(module);
-      //define_UnoSolver(module);
+      define_UnoSolver(module);
    }
 } // namespace
