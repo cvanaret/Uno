73efcac1856ed34f8f05d0a7f3c9185b1e14ea05
Charlie Vanaret <vanaret@zib.de>
2025-07-11 22:39:38 +0200

    First successful version where unopy passes to Uno a function that takes a vector as a parameter. Test in bindings/python/tests/test_UnoSolver.py

--- bindings/python/modules/UnoSolver.cpp
+++ bindings/python/modules/UnoSolver.cpp
@@ -2,6 +2,10 @@
 // Licensed under the MIT license. See LICENSE file in the project directory for details.
 
 #include <pybind11/pybind11.h>
+#include <pybind11/functional.h>
+#include <pybind11/stl.h>
+#include <functional>
+#include <vector>
 #include "Uno.hpp"
 #include "model/Model.hpp"
 #include "optimization/Iterate.hpp"
@@ -9,13 +13,17 @@
 
 namespace py = pybind11;
 
+using objective_type = const std::function<double(const std::vector<double>&)>&;
+
 namespace uno {
    void define_UnoSolver(py::module& module) {
       // Uno class
       py::class_<Uno>(module, "UnoSolver")
          // constructor
-         .def(py::init<size_t, const Options&>(), py::arg("number_constraints"), py::arg("options"), "Constructor")
+         .def(py::init<bool, const Options&>(), py::arg("constrained_model"), py::arg("options"), "Constructor")
          // methods
+         .def("solve", py::overload_cast<size_t, size_t, objective_type>(&Uno::solve),
+            py::arg("number_variables"), py::arg("number_constraints"), py::arg("objective"),"Dummy call")
          .def("solve", py::overload_cast<const Model&, Iterate&, const Options&>(&Uno::solve),
             py::arg("model"), py::arg("iterate"), py::arg("options"),
             "Solve an optimization model with the Uno solver");
