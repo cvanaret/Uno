e481048f3b3aaa5c3b25ea756b7cbeba936bfd35
Charlie Vanaret <vanaret@zib.de>
2025-07-14 15:06:53 +0200

    Implemented hs015 in bindings/python/tests/test_UnoSolver.py and added Jacobian definition

--- bindings/python/tests/test_UnoSolver.py
+++ bindings/python/tests/test_UnoSolver.py
@@ -2,33 +2,48 @@
 # Licensed under the MIT license. See LICENSE file in the project directory for details.
 
 import unopy
+Inf = float("inf")
 
-constrained_model = True
+# hs015.mod
 number_variables = 2
-number_constraints = 1
+number_constraints = 2
+
+variables_bounds = [(-Inf, 0.5), (-Inf, Inf)]
+constraints_bounds = [(1., Inf), (0., Inf)]
 	
 def evaluate_objective(x):
-	return x[0] + x[1]
+	return 100.*(x[1] - x[0]**2)**2 + (1. - x[0])**2
 	
-def evaluate_constraints(x, c):
-	c[0] = x[0]**2 - x[1]
+def evaluate_constraints(x, constraints):
+	constraints[0] = x[0]*x[1]
+	constraints[1] = x[0] + x[1]**2
 	
-def evaluate_objective_gradient(x, g):
-	g.insert(0, 1.)
-	g.insert(1, 1.)
-	
-def evaluate_hessian(x, y, rho, hessian):
-	hessian.insert(100., 0, 0)
-	hessian.insert(200., 0, 1)
-	hessian.insert(300., 1, 1)
+def evaluate_objective_gradient(x, gradient):
+	gradient.insert(0, 400.*x[0]*(x[0]**2 - x[0]) + 2.*x[0] - 2.)
+	gradient.insert(1, 200.*(x[1] - x[0]**2))
+
+def evaluate_jacobian(x, jacobian):
+	# c0
+	jacobian.insert(x[1], 0, 0)
+	jacobian.insert(x[0], 1, 0)
+	# c1
+	jacobian.insert(1., 0, 1)
+	jacobian.insert(2.*x[1], 1, 1)
+
+def evaluate_hessian(x, objective_multiplier, y, hessian):
+	hessian.insert(objective_multiplier*(1200*x[0]**2 - 400.*x[2] + 2.), 0, 0)
+	hessian.insert(-400.*objective_multiplier*x[0] + y[0], 0, 1)
+	hessian.insert(200.*objective_multiplier + 2.*y[1], 1, 1)
 
 if __name__ == '__main__':
 	options = unopy.Options.get_default()
-	unopy.Options.set_preset(options, "filtersqp")
-	solver = unopy.UnoSolver(constrained_model, options)
-	solver.solve(number_variables, number_constraints,
+	unopy.set_preset(options, "filtersqp")
+	constrained_model = (0 < number_constraints)
+	uno_solver = unopy.UnoSolver(constrained_model, options)
+	result = uno_solver.solve(number_variables, number_constraints,
 		evaluate_objective,
 		evaluate_constraints,
 		evaluate_objective_gradient,
+		evaluate_jacobian,
 		evaluate_hessian,
 		options)
