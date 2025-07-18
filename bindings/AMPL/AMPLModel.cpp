f267882db6fdabc2f1959e6726abc35350b9b8d4
Charlie Vanaret <vanaret@zib.de>
2025-07-18 12:48:34 +0200

    Added note for constraints with no bounds

--- bindings/AMPL/AMPLModel.cpp
+++ bindings/AMPL/AMPLModel.cpp
@@ -357,6 +357,7 @@ namespace uno {
          }
          else if (!is_finite(lower_bound) && !is_finite(upper_bound)) {
             WARNING << "Constraint c" << constraint_index << " has no bounds\n";
+            // count the constraint as inequality to avoid reindexing of the constraints
             this->inequality_constraints.emplace_back(constraint_index);
          }
          else {
