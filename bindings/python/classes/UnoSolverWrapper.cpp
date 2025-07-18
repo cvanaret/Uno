204b2e3086b7d1d4ff261ca1f2623940922f0205
Charlie Vanaret <vanaret@zib.de>
2025-07-18 16:43:00 +0200

    Factored out variables and constraints partitions to Model.cpp. Model reformulation added bindings/python/classes/UnoSolverWrapper.cpp

--- bindings/python/classes/UnoSolverWrapper.cpp
+++ bindings/python/classes/UnoSolverWrapper.cpp
@@ -3,6 +3,7 @@
 
 #include "UnoSolverWrapper.hpp"
 #include "PythonModel.hpp"
+#include "model/ModelFactory.hpp"
 #include "options/Options.hpp"
 
 namespace uno {
@@ -18,25 +19,30 @@ namespace uno {
          const std::vector<double>& primal_initial_point, const std::vector<double>& dual_initial_point,
          const Options& options) {
       // create the model
-      const PythonModel model("python_model", number_variables, number_constraints,
+      std::unique_ptr<Model> python_model = std::make_unique<PythonModel>("python_model", number_variables, number_constraints,
          1., evaluate_objective, evaluate_constraints, evaluate_objective_gradient, evaluate_jacobian,
          evaluate_lagrangian_hessian, variables_lower_bounds, variables_upper_bounds, constraints_lower_bounds,
          constraints_upper_bounds, primal_initial_point, dual_initial_point);
+      // reformulate (scale, add slacks, relax the bounds, ...) if necessary
+      std::unique_ptr<Model> model = ModelFactory::reformulate(std::move(python_model), options);
+      DISCRETE << "Reformulated model " << model->name << '\n' << model->number_variables << " variables, " <<
+         model->number_constraints << " constraints (" << model->get_equality_constraints().size() <<
+         " equality, " << model->get_inequality_constraints().size() << " inequality)\n";
 
       Logger::set_logger(options.get_string("logger"));
-      DISCRETE << "Original model " << model.name << '\n' << model.number_variables << " variables, " <<
-         model.number_constraints << " constraints (" << model.get_equality_constraints().size() <<
-         " equality, " << model.get_inequality_constraints().size() << " inequality)\n";
+      DISCRETE << "Original model " << model->name << '\n' << model->number_variables << " variables, " <<
+         model->number_constraints << " constraints (" << model->get_equality_constraints().size() <<
+         " equality, " << model->get_inequality_constraints().size() << " inequality)\n";
 
       // initialize initial primal and dual points
       Iterate initial_iterate(number_variables, number_constraints);
-      model.initial_primal_point(initial_iterate.primals);
-      model.project_onto_variable_bounds(initial_iterate.primals);
-      model.initial_dual_point(initial_iterate.multipliers.constraints);
+      model->initial_primal_point(initial_iterate.primals);
+      model->project_onto_variable_bounds(initial_iterate.primals);
+      model->initial_dual_point(initial_iterate.multipliers.constraints);
       initial_iterate.feasibility_multipliers.reset();
 
       // solve the instance
-      const Result result = this->uno_solver.solve(model, initial_iterate, options);
+      const Result result = this->uno_solver.solve(*model, initial_iterate, options);
       if (result.optimization_status == OptimizationStatus::SUCCESS) {
          // check result.solution.status
       }
