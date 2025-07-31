31ac185269d029ac0dab7227be8291408d8704c5
Charlie Vanaret <vanaret@zib.de>
2025-08-01 01:40:44 +0200

    Fixed errors after merge + wrote dot() function taking a double* as second parameter

--- uno/model/Model.cpp
+++ uno/model/Model.cpp
@@ -9,6 +9,7 @@
 #include "tools/Logger.hpp"
 
 namespace uno {
+   // abstract Problem class
    Model::Model(std::string name, size_t number_variables, size_t number_constraints, double objective_sign) :
          name(std::move(name)), number_variables(number_variables), number_constraints(number_constraints),
          optimization_sense(objective_sign) {
@@ -16,8 +17,7 @@ namespace uno {
 
    void Model::project_onto_variable_bounds(Vector<double>& x) const {
       for (size_t variable_index: Range(this->number_variables)) {
-         x[variable_index] = std::max(std::min(x[variable_index], this->variable_upper_bound(variable_index)),
-            this->variable_lower_bound(variable_index));
+         x[variable_index] = std::max(std::min(x[variable_index], this->variable_upper_bound(variable_index)), this->variable_lower_bound(variable_index));
       }
    }
 
