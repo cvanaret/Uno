740fab72258ac5abe9c4291673e40495bf5b8252
Charlie Vanaret <vanaret@zib.de>
2025-08-14 15:31:21 +0200

    Simplified WarmstartInformation

--- uno/optimization/WarmstartInformation.cpp
+++ uno/optimization/WarmstartInformation.cpp
@@ -5,13 +5,6 @@
 #include "WarmstartInformation.hpp"
 
 namespace uno {
-   void WarmstartInformation::display() const {
-      std::cout << "Objective changed: " << std::boolalpha << this->objective_changed << '\n';
-      std::cout << "Constraints changed: " << std::boolalpha << this->constraints_changed << '\n';
-      std::cout << "Constraint bounds changed: " << std::boolalpha << this->constraint_bounds_changed << '\n';
-      std::cout << "Variable bounds changed: " << std::boolalpha << this->variable_bounds_changed << '\n';
-   }
-
    void WarmstartInformation::no_changes() {
       this->objective_changed = false;
       this->constraints_changed = false;
@@ -19,24 +12,17 @@ namespace uno {
       this->variable_bounds_changed = false;
    }
 
-   void WarmstartInformation::iterate_changed() {
+   void WarmstartInformation::reset() {
       this->objective_changed = true;
       this->constraints_changed = true;
       this->constraint_bounds_changed = true;
       this->variable_bounds_changed = true;
    }
 
-   void WarmstartInformation::whole_problem_changed() {
-      this->objective_changed = true;
-      this->constraints_changed = true;
-      this->constraint_bounds_changed = true;
-      this->variable_bounds_changed = true;
-   }
-
-   void WarmstartInformation::only_objective_changed() {
-      this->objective_changed = true;
-      this->constraints_changed = false;
-      this->constraint_bounds_changed = false;
-      this->variable_bounds_changed = false;
+   void WarmstartInformation::display() const {
+      std::cout << "Objective changed: " << std::boolalpha << this->objective_changed << '\n';
+      std::cout << "Constraints changed: " << std::boolalpha << this->constraints_changed << '\n';
+      std::cout << "Constraint bounds changed: " << std::boolalpha << this->constraint_bounds_changed << '\n';
+      std::cout << "Variable bounds changed: " << std::boolalpha << this->variable_bounds_changed << '\n';
    }
 } // namespace
