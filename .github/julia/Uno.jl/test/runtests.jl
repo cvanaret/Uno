eacfe2a8e1d1f8aa13908a6f817ca84565852fb9
Charlie Vanaret <vanaret@zib.de>
2025-08-19 19:31:38 +0200

    Prefixed calls in runtests.jl

--- .github/julia/Uno.jl/test/runtests.jl
+++ .github/julia/Uno.jl/test/runtests.jl
@@ -2,6 +2,6 @@ using Uno, NLPModels, CUTEst
 
 nlp = CUTEstModel{Float64}("HS15")
 uno_model = uno(nlp)
-uno_set_solver_preset(uno_model, "ipopt")
-uno_set_solver_option(uno_model, "unbounded_objective_threshold", "-1e15")
-uno_optimize(uno_model)
+Uno.uno_set_solver_preset(uno_model, "ipopt")
+Uno.uno_set_solver_option(uno_model, "unbounded_objective_threshold", "-1e15")
+Uno.uno_optimize(uno_model)
