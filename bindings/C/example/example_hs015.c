35d9c004f2113590bd42e53079011152cd8eca70
Charlie Vanaret <vanaret@zib.de>
2025-08-19 15:33:44 +0200

    Added example draft (compiles)

--- bindings/C/example/example_hs015.c
+++ bindings/C/example/example_hs015.c
@@ -49,7 +49,7 @@ int main() {
 	int32_t uno_major, uno_minor, uno_patch;
 	uno_get_version(&uno_major, &uno_minor, &uno_patch);
 	printf("Uno v%d.%d.%d\n", uno_major, uno_minor, uno_patch);
-	
+
 	// model creation
 	const int32_t base_indexing = UNO_ZERO_BASED_INDEXING;
 	// variables
