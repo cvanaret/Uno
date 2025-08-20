157dfb9d723c2fbdc353cc42feb2213c5999a76e
Alexis Montoison <alexis.montoison@polymtl.ca>
2025-08-20 15:18:37 -0500

    Add new tests for Uno.jl

--- .github/julia/Uno.jl/test/runtests.jl
+++ .github/julia/Uno.jl/test/runtests.jl
@@ -1,5 +1,8 @@
 using Uno, NLPModels, CUTEst
 
+version = Uno.version()
+println("The version of Uno is $version.")
+
 nlp = CUTEstModel{Float64}("HS15")
 uno_model = uno(nlp)
 Uno.uno_set_solver_preset(uno_model, "ipopt")
@@ -13,9 +16,14 @@ solution_primal_feasibility = Uno.uno_get_solution_primal_feasibility(uno_model)
 solution_dual_feasibility = Uno.uno_get_solution_dual_feasibility(uno_model)
 solution_complementarity = Uno.uno_get_solution_complementarity(uno_model)
 
-# Should be Julia vectors and not C pointers!
-# Just check that we can call the C routines in Julia.
-#Uno.uno_get_primal_solution(uno_model)
-#Uno.uno_get_constraint_dual_solution(uno_model)
-#Uno.uno_get_lower_bound_dual_solution(uno_model)
-#Uno.uno_get_upper_bound_dual_solution(uno_model)
+primal_solution = Vector{Float64}(undef, nlp.meta.nvar)
+Uno.uno_get_primal_solution(uno_model, primal_solution)
+
+constraint_dual_solution = Vector{Float64}(undef, nlp.meta.ncon)
+Uno.uno_get_constraint_dual_solution(uno_model, constraint_dual_solution)
+
+lower_bound_dual_solution = Vector{Float64}(undef, nlp.meta.nvar)
+Uno.uno_get_lower_bound_dual_solution(uno_model, lower_bound_dual_solution)
+
+upper_bound_dual_solution = Vector{Float64}(undef, nlp.meta.nvar)
+Uno.uno_get_upper_bound_dual_solution(uno_model, upper_bound_dual_solution)
