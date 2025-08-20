6075f972a3ae02818fcdcbf8680608a710cc7e6c
Charlie Vanaret <vanaret@zib.de>
2025-08-20 16:45:44 +0200

    Set logger to DEBUG

--- .github/julia/Uno.jl/test/runtests.jl
+++ .github/julia/Uno.jl/test/runtests.jl
@@ -3,6 +3,8 @@ using Uno, NLPModels, CUTEst
 nlp = CUTEstModel{Float64}("HS15")
 uno_model = uno(nlp)
 Uno.uno_set_solver_preset(uno_model, "ipopt")
+Uno.uno_set_solver_option(uno_model, "logger", "DEBUG")
+Uno.uno_set_solver_option(uno_model, "print_subproblem", "yes")
 Uno.uno_set_solver_option(uno_model, "unbounded_objective_threshold", "-1e15")
 Uno.uno_optimize(uno_model)
 
