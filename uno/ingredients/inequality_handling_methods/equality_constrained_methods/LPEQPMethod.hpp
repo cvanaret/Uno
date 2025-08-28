9d710e64cb448b674e67c2d0b8adbfe366351e02
Charlie Vanaret <vanaret@zib.de>
2025-08-28 18:15:20 +0200

    Fixed rebasing

--- uno/ingredients/inequality_handling_methods/equality_constrained_methods/LPEQPMethod.hpp
+++ uno/ingredients/inequality_handling_methods/equality_constrained_methods/LPEQPMethod.hpp
@@ -17,27 +17,32 @@ namespace uno {
    public:
       explicit LPEQPMethod(const Options& options);
 
-      void initialize(const OptimizationProblem& problem, const HessianModel& hessian_model,
-         RegularizationStrategy<double>& regularization_strategy) override;
+      void initialize(const OptimizationProblem& problem, Iterate& current_iterate,
+         HessianModel& hessian_model, RegularizationStrategy<double>& regularization_strategy, double trust_region_radius) override;
       void initialize_statistics(Statistics& statistics, const Options& options) override;
       void generate_initial_iterate(const OptimizationProblem& problem, Iterate& initial_iterate) override;
       void solve(Statistics& statistics, const OptimizationProblem& problem, Iterate& current_iterate,
-         const Multipliers& current_multipliers, Direction& direction, HessianModel& hessian_model,
-         RegularizationStrategy<double>& regularization_strategy, double trust_region_radius,
-         WarmstartInformation& warmstart_information) override;
+         Direction& direction, HessianModel& hessian_model, RegularizationStrategy<double>& regularization_strategy,
+         double trust_region_radius, WarmstartInformation& warmstart_information) override;
 
       void initialize_feasibility_problem(const l1RelaxedProblem& problem, Iterate& current_iterate) override;
       void exit_feasibility_problem(const OptimizationProblem& problem, Iterate& trial_iterate) override;
       void set_elastic_variable_values(const l1RelaxedProblem& problem, Iterate& current_iterate) override;
       [[nodiscard]] double proximal_coefficient() const override;
 
+      // matrix computations
+      [[nodiscard]] EvaluationSpace& get_evaluation_space() const override;
+      void evaluate_constraint_jacobian(const OptimizationProblem& problem, Iterate& iterate) override;
+      void compute_constraint_jacobian_vector_product(const Vector<double>& vector, Vector<double>& result) const override;
+      void compute_constraint_jacobian_transposed_vector_product(const Vector<double>& vector, Vector<double>& result) const override;
+      [[nodiscard]] double compute_hessian_quadratic_product(const Vector<double>& vector) const override;
+
       // progress measures
-      [[nodiscard]] double hessian_quadratic_product(const Vector<double>& vector) const override;
       void set_auxiliary_measure(const OptimizationProblem& problem, Iterate& iterate) override;
       [[nodiscard]] double compute_predicted_auxiliary_reduction_model(const OptimizationProblem& problem,
          const Iterate& iterate, const Vector<double>& primal_direction, double step_length) const override;
 
-      void postprocess_iterate(const OptimizationProblem& problem, Vector<double>& primals, Multipliers& multipliers) override;
+      void postprocess_iterate(const OptimizationProblem& problem, Iterate& iterate) override;
 
       void set_initial_point(const Vector<double>& initial_point) override;
 
