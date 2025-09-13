f2be52ab2d88323ed3c1f4cf42f57a398c01fa16
Charlie Vanaret <vanaret@zib.de>
2025-09-14 00:53:35 +0200

    Exclude matrix configurations + print sys.version

--- .github/Python/print_platform.py
+++ .github/Python/print_platform.py
@@ -2,5 +2,6 @@ import platform
 import sysconfig
 
 print(platform.machine()) # x86_64 / i386 / arm64 / AMD64
-print(platform.system()) # linux / Darwin
-print(sysconfig.get_platform()) # linux-x86_64
+print(platform.system()) # linux / Darwin / Windows
+print(sysconfig.get_platform()) # linux-x86_64 / macosx-10.13-universal2 / win-amd64
+print(sys.version) # 3.12.3 (main, Aug 14 2025, 17:47:21) [GCC 13.3.0]
