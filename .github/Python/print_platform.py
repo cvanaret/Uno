fadc485461203ff87db4876e4725aad9672b0d80
Charlie Vanaret <vanaret@zib.de>
2025-09-14 15:37:44 +0200

    Updated print_platform.py

--- .github/Python/print_platform.py
+++ .github/Python/print_platform.py
@@ -3,6 +3,7 @@ import platform
 import sysconfig
 
 print(platform.machine()) # x86_64 / i386 / arm64 / AMD64
-print(platform.system()) # linux / Darwin / Windows
+print(platform.system()) # Linux / Darwin / Windows
+print(sys.platform) # linux
 print(sysconfig.get_platform()) # linux-x86_64 / macosx-10.13-universal2 / win-amd64
 print(sys.version) # 3.12.3 (main, Aug 14 2025, 17:47:21) [GCC 13.3.0]
