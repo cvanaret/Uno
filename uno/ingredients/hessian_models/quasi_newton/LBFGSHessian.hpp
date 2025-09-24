621d82bf50e54f2b5b897a275899fb877f84302d
Charlie Vanaret <vanaret@zib.de>
2025-09-24 17:43:12 +0200

    Compiles but is broken, since we now need the inequality handling strategy/its evaluation space to evaluate the Lagrangian

--- uno/ingredients/hessian_models/quasi_newton/LBFGSHessian.hpp
+++ uno/ingredients/hessian_models/quasi_newton/LBFGSHessian.hpp
@@ -21,18 +21,19 @@ namespace uno {
       LBFGSHessian(double objective_multiplier, const Options& options);
       ~LBFGSHessian() override = default;
 
-      [[nodiscard]] bool has_implicit_representation() const override;
-      [[nodiscard]] bool has_explicit_representation() const override;
+      [[nodiscard]] bool has_hessian_operator(const Model& model) const override;
+      [[nodiscard]] bool has_hessian_matrix(const Model& model) const override;
       [[nodiscard]] bool has_curvature(const Model& model) const override;
       [[nodiscard]] size_t number_nonzeros(const Model& model) const override;
+      void compute_sparsity(const Model& model, int* row_indices, int* column_indices, int solver_indexing) const override;
       [[nodiscard]] bool is_positive_definite() const override;
 
       void initialize(const Model& model) override;
       void initialize_statistics(Statistics& statistics, const Options& options) const override;
       void notify_accepted_iterate(const Model& model, Iterate& current_iterate, Iterate& trial_iterate) override;
       void evaluate_hessian(Statistics& statistics, const Model& model, const Vector<double>& primal_variables,
-         double objective_multiplier, const Vector<double>& constraint_multipliers, SymmetricMatrix<size_t, double>& hessian) override;
-      void compute_hessian_vector_product(const Model& model, const double* vector, double objective_multiplier,
+         double objective_multiplier, const Vector<double>& constraint_multipliers, double* hessian_values) override;
+      void compute_hessian_vector_product(const Model& model, const double* x, const double* vector, double objective_multiplier,
          const Vector<double>& constraint_multipliers, double* result) override;
       [[nodiscard]] std::string get_name() const override;
 
