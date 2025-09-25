1c23a37b652d48e0cc3fe095df33c7e3fd4bd802
Alexis Montoison <35051714+amontoison@users.noreply.github.com>
2025-09-25 10:20:13 -0500

    Update UnoMathOptInterfaceExt.jl

--- bindings/Julia/Uno.jl/ext/UnoMathOptInterfaceExt.jl
+++ bindings/Julia/Uno.jl/ext/UnoMathOptInterfaceExt.jl
@@ -3,10 +3,10 @@ module UnoMathOptInterfaceExt
 import Uno
 import MathOptInterface as MOI
 
-function __init__()
-    setglobal!(Uno, :Optimizer, Optimizer)
-    return
-end
+# function __init__()
+#     setglobal!(Uno, :Optimizer, Optimizer)
+#     return
+# end
 
 # include("MOI_utils.jl")
 #include("MOI_wrapper.jl")
