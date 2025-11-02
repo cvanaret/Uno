bd792f9244f4cc0f48cda7daf93c7c8965a00d40
Alexis Montoison <35051714+amontoison@users.noreply.github.com>
2025-11-01 20:06:52 -0500

    [UnoSolver.jl] Add a generic function uno (#403)

--- interfaces/Julia/src/UnoSolver.jl
+++ interfaces/Julia/src/UnoSolver.jl
@@ -13,7 +13,7 @@ function __init__()
     return
 end
 
-export uno_model, uno_solver, uno_optimize, uno_version
+export uno, uno_model, uno_solver, uno_optimize, uno_version
 export uno_set_solver_integer_option, uno_set_solver_double_option
 export uno_set_solver_bool_option, uno_set_solver_string_option
 export uno_set_solver_preset
--- interfaces/Julia/test/NLP_wrapper.jl
+++ interfaces/Julia/test/NLP_wrapper.jl
@@ -1,33 +1,69 @@
-nlp = CUTEstModel{Float64}("HS15")
-model = uno_model(nlp)
-solver = uno_solver(preset="funnelsqp", print_solution=true)
-uno_optimize(solver, model)
+@testset "uno_model -- uno_solver -- uno_optimize" begin
+    nlp = CUTEstModel{Float64}("HS15")
+    model = uno_model(nlp)
+    solver = uno_solver(preset="funnelsqp", print_solution=true)
+    uno_optimize(solver, model)
 
-optimization_status = UnoSolver.uno_get_optimization_status(solver)
-solution_status = UnoSolver.uno_get_solution_status(solver)
-solution_objective = UnoSolver.uno_get_solution_objective(solver)
-solution_primal_feasibility = UnoSolver.uno_get_solution_primal_feasibility(solver)
-solution_stationarity = UnoSolver.uno_get_solution_stationarity(solver)
-solution_complementarity = UnoSolver.uno_get_solution_complementarity(solver)
+    optimization_status = UnoSolver.uno_get_optimization_status(solver)
+    solution_status = UnoSolver.uno_get_solution_status(solver)
+    solution_objective = UnoSolver.uno_get_solution_objective(solver)
+    solution_primal_feasibility = UnoSolver.uno_get_solution_primal_feasibility(solver)
+    solution_stationarity = UnoSolver.uno_get_solution_stationarity(solver)
+    solution_complementarity = UnoSolver.uno_get_solution_complementarity(solver)
 
-primal_solution = Vector{Float64}(undef, nlp.meta.nvar)
-UnoSolver.uno_get_primal_solution(solver, primal_solution)
+    primal_solution = Vector{Float64}(undef, nlp.meta.nvar)
+    UnoSolver.uno_get_primal_solution(solver, primal_solution)
 
-constraint_dual_solution = Vector{Float64}(undef, nlp.meta.ncon)
-UnoSolver.uno_get_constraint_dual_solution(solver, constraint_dual_solution)
+    constraint_dual_solution = Vector{Float64}(undef, nlp.meta.ncon)
+    UnoSolver.uno_get_constraint_dual_solution(solver, constraint_dual_solution)
 
-lower_bound_dual_solution = Vector{Float64}(undef, nlp.meta.nvar)
-UnoSolver.uno_get_lower_bound_dual_solution(solver, lower_bound_dual_solution)
+    lower_bound_dual_solution = Vector{Float64}(undef, nlp.meta.nvar)
+    UnoSolver.uno_get_lower_bound_dual_solution(solver, lower_bound_dual_solution)
 
-upper_bound_dual_solution = Vector{Float64}(undef, nlp.meta.nvar)
-UnoSolver.uno_get_upper_bound_dual_solution(solver, upper_bound_dual_solution)
+    upper_bound_dual_solution = Vector{Float64}(undef, nlp.meta.nvar)
+    UnoSolver.uno_get_upper_bound_dual_solution(solver, upper_bound_dual_solution)
 
-for i in 1:nlp.meta.nvar
-    @test primal_solution[i] == UnoSolver.uno_get_primal_solution_component(solver, i-1)
-    @test lower_bound_dual_solution[i] == UnoSolver.uno_get_lower_bound_dual_solution_component(solver, i-1)
-    @test upper_bound_dual_solution[i] == UnoSolver.uno_get_upper_bound_dual_solution_component(solver, i-1)
+    for i in 1:nlp.meta.nvar
+        @test primal_solution[i] == UnoSolver.uno_get_primal_solution_component(solver, i-1)
+        @test lower_bound_dual_solution[i] == UnoSolver.uno_get_lower_bound_dual_solution_component(solver, i-1)
+        @test upper_bound_dual_solution[i] == UnoSolver.uno_get_upper_bound_dual_solution_component(solver, i-1)
+    end
+
+    for j in 1:nlp.meta.ncon
+        @test constraint_dual_solution[j] == UnoSolver.uno_get_constraint_dual_solution_component(solver, j-1)
+    end
 end
 
-for j in 1:nlp.meta.ncon
-    @test constraint_dual_solution[j] == UnoSolver.uno_get_constraint_dual_solution_component(solver, j-1)
+@testset "uno" begin
+    nlp = CUTEstModel{Float64}("BYRDSPHR")
+    model, solver = uno(nlp, preset="funnelsqp", print_solution=true)
+
+    optimization_status = UnoSolver.uno_get_optimization_status(solver)
+    solution_status = UnoSolver.uno_get_solution_status(solver)
+    solution_objective = UnoSolver.uno_get_solution_objective(solver)
+    solution_primal_feasibility = UnoSolver.uno_get_solution_primal_feasibility(solver)
+    solution_stationarity = UnoSolver.uno_get_solution_stationarity(solver)
+    solution_complementarity = UnoSolver.uno_get_solution_complementarity(solver)
+
+    primal_solution = Vector{Float64}(undef, nlp.meta.nvar)
+    UnoSolver.uno_get_primal_solution(solver, primal_solution)
+
+    constraint_dual_solution = Vector{Float64}(undef, nlp.meta.ncon)
+    UnoSolver.uno_get_constraint_dual_solution(solver, constraint_dual_solution)
+
+    lower_bound_dual_solution = Vector{Float64}(undef, nlp.meta.nvar)
+    UnoSolver.uno_get_lower_bound_dual_solution(solver, lower_bound_dual_solution)
+
+    upper_bound_dual_solution = Vector{Float64}(undef, nlp.meta.nvar)
+    UnoSolver.uno_get_upper_bound_dual_solution(solver, upper_bound_dual_solution)
+
+    for i in 1:nlp.meta.nvar
+        @test primal_solution[i] == UnoSolver.uno_get_primal_solution_component(solver, i-1)
+        @test lower_bound_dual_solution[i] == UnoSolver.uno_get_lower_bound_dual_solution_component(solver, i-1)
+        @test upper_bound_dual_solution[i] == UnoSolver.uno_get_upper_bound_dual_solution_component(solver, i-1)
+    end
+
+    for j in 1:nlp.meta.ncon
+        @test constraint_dual_solution[j] == UnoSolver.uno_get_constraint_dual_solution_component(solver, j-1)
+    end
 end
