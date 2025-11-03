ea52faf16f90bd929d43a71d74f13395c9aaa6b7
Alexis Montoison <alexis.montoison@polymtl.ca>
2025-11-03 10:28:54 -0600

    Update the Fortran interface

--- interfaces/Fortran/test_uno.f90
+++ interfaces/Fortran/test_uno.f90
@@ -1,6 +1,6 @@
 program test_uno
    include 'uno.f90'
-   integer(c_int32_t) :: major, minor, patch
+   integer(uno_int) :: major, minor, patch
    call uno_get_version(major, minor, patch)
    print *, 'Uno version:', major, '.', minor, '.', patch
 end program test_uno
