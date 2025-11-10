46467149e4b5927fab59e2830368b03ddb57be03
Charlie Vanaret <vanaret@zib.de>
2025-11-10 11:50:02 +0100

    Rename UnconstrainedStrategy into NoRelaxation (#414)
    * Renamed UnconstrainedStrategy into NoRelaxation
    
    * Do not use constraint relaxation strategy if only linear constraints and no trust region
    
    * CI: set loggers to INFO
    
    * Fixed typo. Should be TR instead of LS!
    
    * Fails for small LS step lengths. Revert
    
    * Set logger to SILENT in CI

--- uno/ingredients/constraint_relaxation_strategies/ConstraintRelaxationStrategyFactory.cpp
+++ uno/ingredients/constraint_relaxation_strategies/ConstraintRelaxationStrategyFactory.cpp
@@ -1,20 +1,20 @@
-// Copyright (c) 2018-2024 Charlie Vanaret
+// Copyright (c) 2018-2025 Charlie Vanaret
 // Licensed under the MIT license. See LICENSE file in the project directory for details.
 
 #include <string>
 #include "ConstraintRelaxationStrategyFactory.hpp"
 #include "FeasibilityRestoration.hpp"
-#include "UnconstrainedStrategy.hpp"
+#include "NoRelaxation.hpp"
 #include "options/Options.hpp"
 #include "tools/Logger.hpp"
 
 namespace uno {
    std::unique_ptr<ConstraintRelaxationStrategy> ConstraintRelaxationStrategyFactory::create(const Model& model,
          bool use_trust_region, const Options& options) {
-      // set unconstrained strategy automatically
+      // figure out whether we need to relax constraints altogether
       if (model.number_constraints == 0) {
-         INFO << "The model is unconstrained, picking an unconstrained constraint relaxation strategy\n";
-         return std::make_unique<UnconstrainedStrategy>(model, options);
+         INFO << "The model is unconstrained, picking no relaxation\n";
+         return std::make_unique<NoRelaxation>(model, options);
       }
       const std::string constraint_relaxation_type = options.get_string("constraint_relaxation_strategy");
       if (constraint_relaxation_type == "feasibility_restoration") {
