634a96cba283c07c6a6196e214d269493bd3a9e7
Charlie Vanaret <vanaret@zib.de>
2025-11-21 02:07:21 +0100

    Moved MOI tests before NLP tests

--- interfaces/Julia/test/runtests.jl
+++ interfaces/Julia/test/runtests.jl
@@ -9,10 +9,10 @@ println("The version of Uno is $version.")
   include("C_wrapper.jl")
 end
 
-@testset "Interface for NLPModels.jl" begin
-  include("NLP_wrapper.jl")
-end
-
 @testset "Interface for MathOptInterface.jl" begin
   include("MOI_wrapper.jl")
+end
+
+@testset "Interface for NLPModels.jl" begin
+  include("NLP_wrapper.jl")
 end
