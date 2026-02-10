9ebdfb7fad956974dd8b1265db8e3377fe16f12f
Charlie Vanaret <vanaret@zib.de>
2026-02-10 15:50:49 +0100

    Fix of build_tarballs_*.jl

--- .github/julia/build_tarballs_release.jl
+++ .github/julia/build_tarballs_release.jl
@@ -87,12 +87,6 @@ cd $WORKSPACE/srcdir/Uno
 mkdir -p build
 cd build
 
-if [[ "${target}" == *mingw* ]]; then
-    HIGHS_DIR=${prefix}/lib
-else
-    HIGHS_DIR=${libdir}
-fi
-
 if [[ "${target}" == *apple* ]] || [[ "${target}" == *freebsd* ]]; then
     OMP=omp
 else
@@ -105,7 +99,7 @@ cmake \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
     -DCMAKE_BUILD_TYPE=Release \
     -DAMPLSOLVER=${libdir}/libasl.${dlext} \
-    -DHIGHS=${HIGHS_DIR}/libhighs.${dlext} \
+    -DHIGHS=${libdir}/libhighs.${dlext} \
     -DBQPD=${prefix}/lib/libbqpd.a \
     -DHSL=${libdir}/libhsl.${dlext} \
     -DMUMPS_INCLUDE_DIR=${includedir} \
