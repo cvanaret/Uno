9f2d2608f585fc55db6eca111ae306bb4928268f
Charlie Vanaret <vanaret@zib.de>
2026-02-17 11:17:17 +0100

    If no Hessian provided for QPs and NLPs, pick L-BFGS

--- uno/ingredients/hessian_models/HessianModelFactory.cpp
+++ uno/ingredients/hessian_models/HessianModelFactory.cpp
@@ -6,9 +6,9 @@
 #include "HessianModelFactory.hpp"
 #include "HessianModel.hpp"
 #include "ExactHessian.hpp"
+#include "quasi_newton/LBFGSHessian.hpp"
 #include "IdentityHessian.hpp"
 #include "ZeroHessian.hpp"
-#include "quasi_newton/LBFGSHessian.hpp"
 #include "model/Model.hpp"
 #include "options/Options.hpp"
 #include "tools/Logger.hpp"
@@ -16,25 +16,32 @@
 namespace uno {
    std::unique_ptr<HessianModel> HessianModelFactory::create(const Model& model, [[maybe_unused]] double objective_multiplier,
          const Options& options) {
+      // first look at the problem type. If it is an LP, pick zero Hessian model
+      if (model.get_problem_type() == ProblemType::LINEAR) {
+         return std::make_unique<ZeroHessian>(model.number_variables);
+      }
+
+      // then look at the option hessian_model
+      // from now onwards, the problem is nonlinear (QP or NLP)
       const std::string& hessian_model = options.get_string("hessian_model");
       if (hessian_model == "exact") {
-         // if no Hessian (matrix or operator) is available, pick a zero Hessian
-         if (!model.has_hessian_matrix() && !model.has_hessian_operator()) {
-            WARNING << "An exact Hessian (matrix or operator) was not provided, setting a zero Hessian instead\n";
-            return std::make_unique<ZeroHessian>(model.number_variables);
-         }
-         else {
+         if (model.has_hessian_matrix() || model.has_hessian_operator()) {
             return std::make_unique<ExactHessian>(model);
          }
+         // if no Hessian (matrix or operator) is available, pick an L-BFGS Hessian
+         else {
+            WARNING << "An exact Hessian (matrix or operator) was not provided, setting an L-BFGS Hessian instead\n";
+            return std::make_unique<LBFGSHessian>(model, objective_multiplier, options);
+         }
+      }
+      else if (hessian_model == "LBFGS") {
+         return std::make_unique<LBFGSHessian>(model, objective_multiplier, options);
       }
       else if (hessian_model == "identity") {
          return std::make_unique<IdentityHessian>(model.number_variables);
       }
       else if (hessian_model == "zero") {
          return std::make_unique<ZeroHessian>(model.number_variables);
-      }
-      else if (hessian_model == "LBFGS") {
-         return std::make_unique<LBFGSHessian>(model, objective_multiplier, options);
       }
       throw std::invalid_argument("Hessian model " + hessian_model + " does not exist");
    }
