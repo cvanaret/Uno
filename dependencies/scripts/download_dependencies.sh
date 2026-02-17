79ac774991753cd970c6183826f6672adb2cd75c
Alexis Montoison <35051714+amontoison@users.noreply.github.com>
2026-02-16 19:35:00 -0600

    [CI] Add a common script build.yml (#515)

--- dependencies/scripts/download_dependencies.sh
+++ dependencies/scripts/download_dependencies.sh
@@ -32,7 +32,7 @@ tar -xzvf BQPD.tar.gz
 pwd
 
 # download UnoUtils: MUMPS (+ METIS, BLAS and LAPACK) and HiGHS
-VERSION="2026.2.11"
+VERSION="2026.2.17"
 REPO="https://github.com/amontoison/UnoUtils_jll.jl/releases/download/UnoUtils-v${VERSION}%2B0"
 ASSET_NAME="UnoUtils.v${VERSION}.${ARCH}-${OS}-libgfortran5.tar.gz"
 ASSET_URL="${REPO}/${ASSET_NAME}"
