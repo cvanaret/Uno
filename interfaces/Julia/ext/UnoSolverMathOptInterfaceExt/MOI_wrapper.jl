d30a3c1db28309c1cbb527e34fe32078ae058a4f
Alexis Montoison <35051714+amontoison@users.noreply.github.com>
2026-03-03 17:51:13 -0600

    Apply suggestion from @amontoison

--- interfaces/Julia/ext/UnoSolverMathOptInterfaceExt/MOI_wrapper.jl
+++ interfaces/Julia/ext/UnoSolverMathOptInterfaceExt/MOI_wrapper.jl
@@ -1670,8 +1670,7 @@ end
 ### MOI.ConstraintDual
 
 function _dual_multiplier(model::Optimizer)
-    return 1.
-    #return xor(model.problem_type == "LP", model.sense == MOI.MAX_SENSE) ? 1.0 : -1.0
+    return model.sense == MOI.MAX_SENSE ? 1.0 : -1.0
 end
 
 function MOI.get(
