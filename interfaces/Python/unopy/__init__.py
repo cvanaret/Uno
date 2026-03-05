02e82f953fb58aa9fd0cdbefecd386a0adce8536
Charlie Vanaret <vanaret@zib.de>
2026-03-05 23:33:48 +0100

    Forgot __init__.py!

---
+++ interfaces/Python/unopy/__init__.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+# load bundled DLLs from unopy.libs
+basedir = os.path.dirname(__file__)
+subdir = os.path.join(basedir, '..', 'unopy.libs')
+if os.name == 'nt':
+    os.add_dll_directory(os.path.abspath(subdir))
+elif sys.platform == 'cygwin':
+    os.environ['PATH'] = os.pathsep.join((os.environ['PATH'], basedir, subdir))
+
+from .unopy import *
