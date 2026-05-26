4516d1407b181ba12e3607425ce18882bca5f981
Charlie Vanaret <vanaret@zib.de>
2026-05-26 22:02:48 +0200

    Missing include

--- uno/ingredients/subproblem_solvers/TRON/TRONSolver.cpp
+++ uno/ingredients/subproblem_solvers/TRON/TRONSolver.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) 2026 Charlie Vanaret
 // Licensed under the MIT license. See LICENSE file in the project directory for details.
 
+#include <algorithm>
 #include <cmath>
 #include <iostream>
 #include <limits>
