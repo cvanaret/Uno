ff5f38b66551171eb35cd32732495124bedc90f0
Charlie Vanaret <vanaret@zib.de>
2026-05-27 19:01:34 +0200

    Added a functional test for libkrylov based on Alexis' example

---
+++ uno/ingredients/subproblem_solvers/Krylov/KrylovSolvers.hpp
@@ -0,0 +1,9 @@
+// Copyright (c) 2026 Charlie Vanaret
+// Licensed under the MIT license. See LICENSE file in the project directory for details.
+
+#ifndef UNO_KRYLOVSOLVERS_H
+#define UNO_KRYLOVSOLVERS_H
+
+#include "krylov.h"
+
+#endif // UNO_KRYLOVSOLVERS_H
