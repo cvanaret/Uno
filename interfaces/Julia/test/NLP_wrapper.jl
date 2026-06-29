c0c3c3ccdc303a31fcc278b47c380acbc0efcc8c
Alexis Montoison <35051714+amontoison@users.noreply.github.com>
2026-06-30 00:01:59 +0200

    [CI] Update the version of HiGHS (#798)
    * [CI] Update HiGHS_jll dependency compatibility version
    
    * Update the version of HiGHS

--- interfaces/Julia/test/NLP_wrapper.jl
+++ interfaces/Julia/test/NLP_wrapper.jl
@@ -1,81 +1,162 @@
-@testset "preset = $preset" for preset in ("filterslp", "filtersqp", "funnelsqp")
-    @testset "uno_model -- uno_solver -- uno_optimize -- HS15 -- $(hessian_model) Hessian" for hessian_model in ("exact", "LBFGS", "LSR1")
-        nlp = CUTEstModel{Float64}("HS15")
-        model = uno_model(nlp)
-        solver = uno_solver(preset=preset, print_solution=true, hessian_model=hessian_model)
-        uno_optimize(solver, model)
+@testset "preset = $preset" for preset in ("filterslp",)
+    @testset "LP solver = $lp" for lp in ("BQPD", "HiGHS")
+        @testset "uno_model -- uno_solver -- uno_optimize -- HS15 -- $(hessian_model) Hessian" for hessian_model in ("exact", "LBFGS", "LSR1")
+            nlp = CUTEstModel{Float64}("HS15")
+            model = uno_model(nlp)
+            solver = uno_solver(preset=preset, print_solution=true, LP_solver=lp, hessian_model=hessian_model)
+            uno_optimize(solver, model)
 
-        optimization_status = UnoSolver.uno_get_optimization_status(solver)
-        solution_status = UnoSolver.uno_get_solution_status(solver)
-        solution_objective = UnoSolver.uno_get_solution_objective(solver)
-        solution_primal_feasibility = UnoSolver.uno_get_solution_primal_feasibility(solver)
-        solution_stationarity = UnoSolver.uno_get_solution_stationarity(solver)
-        solution_complementarity = UnoSolver.uno_get_solution_complementarity(solver)
+            optimization_status = UnoSolver.uno_get_optimization_status(solver)
+            solution_status = UnoSolver.uno_get_solution_status(solver)
+            solution_objective = UnoSolver.uno_get_solution_objective(solver)
+            solution_primal_feasibility = UnoSolver.uno_get_solution_primal_feasibility(solver)
+            solution_stationarity = UnoSolver.uno_get_solution_stationarity(solver)
+            solution_complementarity = UnoSolver.uno_get_solution_complementarity(solver)
 
-        primal_solution = Vector{Float64}(undef, nlp.meta.nvar)
-        UnoSolver.uno_get_primal_solution(solver, primal_solution)
+            primal_solution = Vector{Float64}(undef, nlp.meta.nvar)
+            UnoSolver.uno_get_primal_solution(solver, primal_solution)
 
-        constraint_dual_solution = Vector{Float64}(undef, nlp.meta.ncon)
-        UnoSolver.uno_get_constraint_dual_solution(solver, constraint_dual_solution)
+            constraint_dual_solution = Vector{Float64}(undef, nlp.meta.ncon)
+            UnoSolver.uno_get_constraint_dual_solution(solver, constraint_dual_solution)
 
-        lower_bound_dual_solution = Vector{Float64}(undef, nlp.meta.nvar)
-        UnoSolver.uno_get_lower_bound_dual_solution(solver, lower_bound_dual_solution)
+            lower_bound_dual_solution = Vector{Float64}(undef, nlp.meta.nvar)
+            UnoSolver.uno_get_lower_bound_dual_solution(solver, lower_bound_dual_solution)
 
-        upper_bound_dual_solution = Vector{Float64}(undef, nlp.meta.nvar)
-        UnoSolver.uno_get_upper_bound_dual_solution(solver, upper_bound_dual_solution)
+            upper_bound_dual_solution = Vector{Float64}(undef, nlp.meta.nvar)
+            UnoSolver.uno_get_upper_bound_dual_solution(solver, upper_bound_dual_solution)
 
-        for i in 1:nlp.meta.nvar
-            @test primal_solution[i] == UnoSolver.uno_get_primal_solution_component(solver, i)
-            @test lower_bound_dual_solution[i] == UnoSolver.uno_get_lower_bound_dual_solution_component(solver, i)
-            @test upper_bound_dual_solution[i] == UnoSolver.uno_get_upper_bound_dual_solution_component(solver, i)
+            for i in 1:nlp.meta.nvar
+                @test primal_solution[i] == UnoSolver.uno_get_primal_solution_component(solver, i)
+                @test lower_bound_dual_solution[i] == UnoSolver.uno_get_lower_bound_dual_solution_component(solver, i)
+                @test upper_bound_dual_solution[i] == UnoSolver.uno_get_upper_bound_dual_solution_component(solver, i)
+            end
+
+            for j in 1:nlp.meta.ncon
+                @test constraint_dual_solution[j] == UnoSolver.uno_get_constraint_dual_solution_component(solver, j)
+            end
+
+            finalize(nlp)
         end
 
-        for j in 1:nlp.meta.ncon
-            @test constraint_dual_solution[j] == UnoSolver.uno_get_constraint_dual_solution_component(solver, j)
+        @testset "uno -- CUTEst -- BYRDSPHR -- $(hessian_model) Hessian" for hessian_model in ("exact", "LBFGS", "LSR1")
+            nlp = CUTEstModel{Float64}("BYRDSPHR")
+            stats = uno(nlp, preset=preset, print_solution=true, LP_solver=lp, hessian_model=hessian_model)
+            model, solver = stats.model, stats.solver
+
+            optimization_status = UnoSolver.uno_get_optimization_status(solver)
+            solution_status = UnoSolver.uno_get_solution_status(solver)
+            solution_objective = UnoSolver.uno_get_solution_objective(solver)
+            solution_primal_feasibility = UnoSolver.uno_get_solution_primal_feasibility(solver)
+            solution_stationarity = UnoSolver.uno_get_solution_stationarity(solver)
+            solution_complementarity = UnoSolver.uno_get_solution_complementarity(solver)
+
+            primal_solution = Vector{Float64}(undef, nlp.meta.nvar)
+            UnoSolver.uno_get_primal_solution(solver, primal_solution)
+
+            constraint_dual_solution = Vector{Float64}(undef, nlp.meta.ncon)
+            UnoSolver.uno_get_constraint_dual_solution(solver, constraint_dual_solution)
+
+            lower_bound_dual_solution = Vector{Float64}(undef, nlp.meta.nvar)
+            UnoSolver.uno_get_lower_bound_dual_solution(solver, lower_bound_dual_solution)
+
+            upper_bound_dual_solution = Vector{Float64}(undef, nlp.meta.nvar)
+            UnoSolver.uno_get_upper_bound_dual_solution(solver, upper_bound_dual_solution)
+
+            for i in 1:nlp.meta.nvar
+                @test stats.solution[i] == UnoSolver.uno_get_primal_solution_component(solver, i)
+                @test stats.multipliers_L[i] == UnoSolver.uno_get_lower_bound_dual_solution_component(solver, i)
+                @test stats.multipliers_U[i] == UnoSolver.uno_get_upper_bound_dual_solution_component(solver, i)
+            end
+
+            for j in 1:nlp.meta.ncon
+                @test stats.multipliers[j] == UnoSolver.uno_get_constraint_dual_solution_component(solver, j)
+            end
+
+            finalize(nlp)
         end
-
-        finalize(nlp)
-    end
-
-    @testset "uno -- CUTEst -- BYRDSPHR -- $(hessian_model) Hessian" for hessian_model in ("exact", "LBFGS", "LSR1")
-        nlp = CUTEstModel{Float64}("BYRDSPHR")
-        stats = uno(nlp, preset=preset, print_solution=true, hessian_model=hessian_model)
-        model, solver = stats.model, stats.solver
-
-        optimization_status = UnoSolver.uno_get_optimization_status(solver)
-        solution_status = UnoSolver.uno_get_solution_status(solver)
-        solution_objective = UnoSolver.uno_get_solution_objective(solver)
-        solution_primal_feasibility = UnoSolver.uno_get_solution_primal_feasibility(solver)
-        solution_stationarity = UnoSolver.uno_get_solution_stationarity(solver)
-        solution_complementarity = UnoSolver.uno_get_solution_complementarity(solver)
-
-        primal_solution = Vector{Float64}(undef, nlp.meta.nvar)
-        UnoSolver.uno_get_primal_solution(solver, primal_solution)
-
-        constraint_dual_solution = Vector{Float64}(undef, nlp.meta.ncon)
-        UnoSolver.uno_get_constraint_dual_solution(solver, constraint_dual_solution)
-
-        lower_bound_dual_solution = Vector{Float64}(undef, nlp.meta.nvar)
-        UnoSolver.uno_get_lower_bound_dual_solution(solver, lower_bound_dual_solution)
-
-        upper_bound_dual_solution = Vector{Float64}(undef, nlp.meta.nvar)
-        UnoSolver.uno_get_upper_bound_dual_solution(solver, upper_bound_dual_solution)
-
-        for i in 1:nlp.meta.nvar
-            @test stats.solution[i] == UnoSolver.uno_get_primal_solution_component(solver, i)
-            @test stats.multipliers_L[i] == UnoSolver.uno_get_lower_bound_dual_solution_component(solver, i)
-            @test stats.multipliers_U[i] == UnoSolver.uno_get_upper_bound_dual_solution_component(solver, i)
-        end
-
-        for j in 1:nlp.meta.ncon
-            @test stats.multipliers[j] == UnoSolver.uno_get_constraint_dual_solution_component(solver, j)
-        end
-
-        finalize(nlp)
     end
 end
 
-@testset "preset = ipopt" begin
+@testset "preset = $preset" for preset in ("filtersqp", "funnelsqp")
+    @testset "QP solver = $qp" for qp in ("BQPD",)
+        @testset "uno_model -- uno_solver -- uno_optimize -- HS15 -- $(hessian_model) Hessian" for hessian_model in ("exact", "LBFGS", "LSR1")
+            nlp = CUTEstModel{Float64}("HS15")
+            model = uno_model(nlp)
+            solver = uno_solver(preset=preset, print_solution=true, QP_solver=qp, hessian_model=hessian_model)
+            uno_optimize(solver, model)
+
+            optimization_status = UnoSolver.uno_get_optimization_status(solver)
+            solution_status = UnoSolver.uno_get_solution_status(solver)
+            solution_objective = UnoSolver.uno_get_solution_objective(solver)
+            solution_primal_feasibility = UnoSolver.uno_get_solution_primal_feasibility(solver)
+            solution_stationarity = UnoSolver.uno_get_solution_stationarity(solver)
+            solution_complementarity = UnoSolver.uno_get_solution_complementarity(solver)
+
+            primal_solution = Vector{Float64}(undef, nlp.meta.nvar)
+            UnoSolver.uno_get_primal_solution(solver, primal_solution)
+
+            constraint_dual_solution = Vector{Float64}(undef, nlp.meta.ncon)
+            UnoSolver.uno_get_constraint_dual_solution(solver, constraint_dual_solution)
+
+            lower_bound_dual_solution = Vector{Float64}(undef, nlp.meta.nvar)
+            UnoSolver.uno_get_lower_bound_dual_solution(solver, lower_bound_dual_solution)
+
+            upper_bound_dual_solution = Vector{Float64}(undef, nlp.meta.nvar)
+            UnoSolver.uno_get_upper_bound_dual_solution(solver, upper_bound_dual_solution)
+
+            for i in 1:nlp.meta.nvar
+                @test primal_solution[i] == UnoSolver.uno_get_primal_solution_component(solver, i)
+                @test lower_bound_dual_solution[i] == UnoSolver.uno_get_lower_bound_dual_solution_component(solver, i)
+                @test upper_bound_dual_solution[i] == UnoSolver.uno_get_upper_bound_dual_solution_component(solver, i)
+            end
+
+            for j in 1:nlp.meta.ncon
+                @test constraint_dual_solution[j] == UnoSolver.uno_get_constraint_dual_solution_component(solver, j)
+            end
+
+            finalize(nlp)
+        end
+
+        @testset "uno -- CUTEst -- BYRDSPHR -- $(hessian_model) Hessian" for hessian_model in ("exact", "LBFGS", "LSR1")
+            nlp = CUTEstModel{Float64}("BYRDSPHR")
+            stats = uno(nlp, preset=preset, print_solution=true, QP_solver=qp, hessian_model=hessian_model)
+            model, solver = stats.model, stats.solver
+
+            optimization_status = UnoSolver.uno_get_optimization_status(solver)
+            solution_status = UnoSolver.uno_get_solution_status(solver)
+            solution_objective = UnoSolver.uno_get_solution_objective(solver)
+            solution_primal_feasibility = UnoSolver.uno_get_solution_primal_feasibility(solver)
+            solution_stationarity = UnoSolver.uno_get_solution_stationarity(solver)
+            solution_complementarity = UnoSolver.uno_get_solution_complementarity(solver)
+
+            primal_solution = Vector{Float64}(undef, nlp.meta.nvar)
+            UnoSolver.uno_get_primal_solution(solver, primal_solution)
+
+            constraint_dual_solution = Vector{Float64}(undef, nlp.meta.ncon)
+            UnoSolver.uno_get_constraint_dual_solution(solver, constraint_dual_solution)
+
+            lower_bound_dual_solution = Vector{Float64}(undef, nlp.meta.nvar)
+            UnoSolver.uno_get_lower_bound_dual_solution(solver, lower_bound_dual_solution)
+
+            upper_bound_dual_solution = Vector{Float64}(undef, nlp.meta.nvar)
+            UnoSolver.uno_get_upper_bound_dual_solution(solver, upper_bound_dual_solution)
+
+            for i in 1:nlp.meta.nvar
+                @test stats.solution[i] == UnoSolver.uno_get_primal_solution_component(solver, i)
+                @test stats.multipliers_L[i] == UnoSolver.uno_get_lower_bound_dual_solution_component(solver, i)
+                @test stats.multipliers_U[i] == UnoSolver.uno_get_upper_bound_dual_solution_component(solver, i)
+            end
+
+            for j in 1:nlp.meta.ncon
+                @test stats.multipliers[j] == UnoSolver.uno_get_constraint_dual_solution_component(solver, j)
+            end
+
+            finalize(nlp)
+        end
+    end
+end
+
+@testset "preset = $preset" for preset in ("ipopt",)
     for linear_solver in ("MUMPS", "SSIDS", "MA27", "MA57")
         (linear_solver == "SSIDS") && (!haskey(ENV, "OMP_CANCELLATION") || !haskey(ENV, "OMP_PROC_BIND")) && continue
         (linear_solver == "MA27" || linear_solver == "MA57") && !(@ccall HSL_jll.libhsl.LIBHSL_isfunctional()::Bool) && continue
@@ -84,7 +165,7 @@ end
             @testset "uno_model -- uno_solver -- uno_optimize -- HS15 -- $(hessian_model) Hessian" for hessian_model in ("exact", "LBFGS")
                 nlp = CUTEstModel{Float64}("HS15")
                 model = uno_model(nlp)
-                solver = uno_solver(preset="ipopt", print_solution=true, linear_solver=linear_solver, hessian_model=hessian_model)
+                solver = uno_solver(preset=preset, print_solution=true, linear_solver=linear_solver, hessian_model=hessian_model)
                 uno_optimize(solver, model)
 
                 optimization_status = UnoSolver.uno_get_optimization_status(solver)
@@ -121,7 +202,7 @@ end
 
             @testset "uno -- CUTEst -- BYRDSPHR -- $(hessian_model) Hessian" for hessian_model in ("exact", "LBFGS")
                 nlp = CUTEstModel{Float64}("BYRDSPHR")
-                stats = uno(nlp, preset="ipopt", print_solution=true, linear_solver=linear_solver, hessian_model=hessian_model)
+                stats = uno(nlp, preset=preset, print_solution=true, linear_solver=linear_solver, hessian_model=hessian_model)
                 model, solver = stats.model, stats.solver
 
                 optimization_status = UnoSolver.uno_get_optimization_status(solver)
