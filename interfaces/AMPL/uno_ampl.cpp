8813206a05e9c9b015a7b79c6c5b3407e2ef43fa
Charlie Vanaret <vanaret@zib.de>
2026-08-13 15:39:58 +0200

    Bug fix: set logger before creating model so that warnings do not leak in SILENT mode

--- interfaces/AMPL/uno_ampl.cpp
+++ interfaces/AMPL/uno_ampl.cpp
@@ -88,6 +88,9 @@ int main(int argc, char* argv[]) {
          const AMPLModel model(model_name);
 
          // set the preset (default: auto)
+         // model name
+         const char* model_name = argv[1];
+         const AMPLModel model(model_name);
          Presets::set(model, options, options.get_string("preset"));
 
          // set the rest of the command line options
