008f11ad4641a196c0a30101f8673d80ed7973d5
Charlie Vanaret <vanaret@zib.de>
2026-08-13 15:09:10 +0200

    Fixed prints

--- uno/ingredients/subproblem_solvers/WoodburyEQPSolver.cpp
+++ uno/ingredients/subproblem_solvers/WoodburyEQPSolver.cpp
@@ -80,7 +80,7 @@ namespace uno {
       }
 
       // solve the linear system
-      DEBUG3 << "KKT matrix values: " << linear_system.matrix_values << '\n';
+      DEBUG3 << "KKT matrix values: " << linear_system.matrix_values << "\n\n";
       this->linear_solver->solve_indefinite_system(linear_system.solution.data());
 
       // set the constraint multipliers if their norm is reasonable
@@ -138,7 +138,7 @@ namespace uno {
 
       // solve the linear system with only the diagonal part and store the result in solution_diagonal_part
       Vector<double> solution_diagonal_part(subproblem.number_variables + subproblem.number_constraints);
-      DEBUG3 << "KKT matrix values: " << linear_system.matrix_values << '\n';
+      DEBUG3 << "KKT matrix values: " << linear_system.matrix_values << "\n\n";
       this->linear_solver->solve_indefinite_system(solution_diagonal_part.data());
       if (this->linear_solver->matrix_is_singular()) {
          this->direction.status = SubproblemStatus::INFEASIBLE;
