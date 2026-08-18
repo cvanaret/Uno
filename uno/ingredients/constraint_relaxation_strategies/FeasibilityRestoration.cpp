c9bc419f6ffce9d1ea82b070770e940871902acf
Charlie Vanaret <vanaret@zib.de>
2026-08-18 19:28:40 +0200

    Minor changes

--- uno/ingredients/constraint_relaxation_strategies/FeasibilityRestoration.cpp
+++ uno/ingredients/constraint_relaxation_strategies/FeasibilityRestoration.cpp
@@ -96,7 +96,6 @@ namespace uno {
       // compute the feasibility multipliers and the residuals, and test termination wrt the feasibility problem
       Multipliers tentative_multipliers(current_iterate.multipliers);
       this->feasibility_problem.compute_multipliers(tentative_multipliers, current_evaluations);
-      DEBUG << current_iterate << '\n';
       // this->feasibility_problem.compute_residuals(current_iterate, current_evaluations);
       this->original_problem.model.evaluate_lagrangian_gradient(current_iterate.primals, tentative_multipliers,
          0., current_evaluations, current_iterate.residuals.lagrangian_gradient);
@@ -105,6 +104,7 @@ namespace uno {
       if (norm_inf(view(current_iterate.residuals.lagrangian_gradient, 0, this->original_problem.model.number_variables)) <= 1e-8) {
          current_iterate.status = SolutionStatus::INFEASIBLE_STATIONARY_POINT;
          std::swap(tentative_multipliers, current_iterate.multipliers);
+         DEBUG << current_iterate << '\n';
          DEBUG << "The current iterate is an infeasible stationary point\n";
          return true;
       }
