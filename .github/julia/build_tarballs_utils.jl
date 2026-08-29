e51575b17e3d5f054185cabf393e8bb02ba6774b
Alexis Montoison <35051714+amontoison@users.noreply.github.com>
2026-08-29 11:59:17 -0600

    Apply batched suggestions from code review
    Co-authored-by: Alexis Montoison <35051714+amontoison@users.noreply.github.com>

--- .github/julia/build_tarballs_utils.jl
+++ .github/julia/build_tarballs_utils.jl
@@ -208,19 +208,12 @@ sed -i 's/(*opt)/opt->count() > 0/' extern/cli11/CLI11.hpp
 # fix-destroy.patch
 sed -i 's/Highs::resetGlobalScheduler(true);//' highs/interfaces/highs_c_api.cpp
 
-TLS_FLAG=""
-if [[ "${target}" == *-mingw* ]]; then
-    TLS_FLAG="-fno-emulated-tls"
-fi
-
 mkdir build
 cd build
 cmake .. \
     -DCMAKE_INSTALL_PREFIX=${prefix} \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_C_FLAGS="${TLS_FLAG}" \
-    -DCMAKE_CXX_FLAGS="${TLS_FLAG}" \
     -DBUILD_SHARED_LIBS=OFF \
     -DZLIB=OFF \
     -DHIPO=ON \
