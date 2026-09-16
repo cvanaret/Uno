8edffa36d445c63114752359e24364f3437f9aec
Charlie Vanaret <vanaret@zib.de>
2026-09-16 10:25:02 +0200

    Project the primal variables into the interior only at the initial iterate

--- uno/ingredients/inequality_handling_methods/interior_point_methods/barrier_problems/PrimalDualInteriorPointProblem.cpp
+++ uno/ingredients/inequality_handling_methods/interior_point_methods/barrier_problems/PrimalDualInteriorPointProblem.cpp
@@ -45,19 +45,31 @@ namespace uno {
       return false;
    }
 
-   void PrimalDualInteriorPointProblem::create_iterate(Iterate& iterate, Evaluations& evaluations, bool /*is_initial_iterate*/) const {
-      // make the initial point strictly feasible wrt the bounds
-      bool iterate_changed = false;
-      for (size_t variable_index: Range(this->number_variables)) {
-         const double old_value = iterate.primals[variable_index];
-         iterate.primals[variable_index] = this->push_variable_to_interior(iterate.primals[variable_index],
-            this->variables_lower_bounds[variable_index], this->variables_upper_bounds[variable_index]);
-         if (iterate.primals[variable_index] != old_value) {
-            iterate_changed = true;
+   void PrimalDualInteriorPointProblem::create_iterate(Iterate& iterate, Evaluations& evaluations, bool is_initial_iterate) const {
+      if (is_initial_iterate) {
+         // make the initial point strictly feasible wrt the bounds
+         bool iterate_changed = false;
+         for (size_t variable_index: Range(this->number_variables)) {
+            const double old_value = iterate.primals[variable_index];
+            iterate.primals[variable_index] = this->push_variable_to_interior(iterate.primals[variable_index],
+               this->variables_lower_bounds[variable_index], this->variables_upper_bounds[variable_index]);
+            if (iterate.primals[variable_index] != old_value) {
+               iterate_changed = true;
+            }
          }
-      }
-      if (iterate_changed) {
-         evaluations.reset();
+         if (iterate_changed) {
+            evaluations.reset();
+         }
+
+         // set the bound multipliers
+         for (size_t variable_index: Range(this->inner.number_variables)) {
+            if (is_finite(this->variables_lower_bounds[variable_index])) {
+               iterate.multipliers.lower_bounds[variable_index] = this->parameters.default_multiplier;
+            }
+            if (is_finite(this->variables_upper_bounds[variable_index])) {
+               iterate.multipliers.upper_bounds[variable_index] = -this->parameters.default_multiplier;
+            }
+         }
       }
 
       // set the slack variables (if any)
@@ -70,16 +82,6 @@ namespace uno {
          }
          // since the slacks have been set, the constraints should be updated
          evaluations.are_constraints_computed = false;
-      }
-
-      // set the bound multipliers
-      for (size_t variable_index: Range(this->inner.number_variables)) {
-         if (is_finite(this->variables_lower_bounds[variable_index])) {
-            iterate.multipliers.lower_bounds[variable_index] = this->parameters.default_multiplier;
-         }
-         if (is_finite(this->variables_upper_bounds[variable_index])) {
-            iterate.multipliers.upper_bounds[variable_index] = -this->parameters.default_multiplier;
-         }
       }
    }
 
